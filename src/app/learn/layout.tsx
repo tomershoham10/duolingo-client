ce511f8ecc87366d53d12ab6dc85e3e52092ead1
tomershoham10 <tomershoham10@gmail.com>
2024-03-24 14:10:54 +0200

    style: student view layout sizes fixed

--- src/app/learn/layout.tsx
+++ src/app/learn/layout.tsx
@@ -11,12 +11,14 @@ export default function RootLayout({
   return (
     <div className='flex h-screen w-full flex-row'>
       {/* <Alert /> */}
-      <StudentSideBar />
-      <div className='flex h-screen w-full flex-col'>
-        <div className='flex h-full w-full flex-row justify-between overflow-hidden'>
-          {children}
-          <InfoBar />
-        </div>
+      <div className='basis-1/5 2xl:basis-[15%] 3xl:basis-[10%]'>
+        <StudentSideBar />
+      </div>
+      <div className='basis-3/5 overflow-x-hidden 2xl:basis-[67.5%] 3xl:basis-[77.5%]'>
+        {children}
+      </div>
+      <div className='basis-1/5 2xl:basis-[17.5%] 3xl:basis-[12.5%]'>
+        <InfoBar />
       </div>
     </div>
   );
