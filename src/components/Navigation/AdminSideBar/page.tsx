7ac1c0e391bca3efbba2b90790a3e49e2d4df17e
tomershoham10 <tomershoham10@gmail.com>
2024-04-09 15:12:03 +0300

    refactor: auth middleware

--- src/components/Navigation/AdminSideBar/page.tsx
+++ src/components/Navigation/AdminSideBar/page.tsx
@@ -48,11 +48,6 @@ const AdminSideBar: React.FC = () => {
 
   const sidebarItemRef = useRef<HTMLLIElement | null>(null);
 
-  const useUserStoreObj = {
-    permission: useStore(useUserStore, (state) => state.permission),
-    isLoggedIn: useStore(useUserStore, (state) => state.isLoggedIn),
-  };
-
   const courseName = useStore(useCourseStore, (state) => state.name);
   const coursesList = useStore(useCourseStore, (state) => state.coursesList);
   const useCourseStoreObj = React.useMemo(() => {
@@ -125,12 +120,7 @@ const AdminSideBar: React.FC = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      if (
-        useUserStoreObj.permission === PermissionsTypes.ADMIN &&
-        coursesList &&
-        coursesList.length > 0 &&
-        !!!coursesList[0]._id
-      ) {
+      if (coursesList && coursesList.length > 0 && !!!coursesList[0]._id) {
         try {
           const coursesList = await getCourses();
           coursesList ? useCourseStoreObj.updateCoursesList(coursesList) : null;
@@ -141,7 +131,7 @@ const AdminSideBar: React.FC = () => {
     };
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [coursesList, useUserStoreObj.permission]);
+  }, [coursesList]);
 
   useEffect(() => {
     usePopupStoreObj.selectedPopup === PopupsTypes.CLOSED
@@ -185,26 +175,23 @@ const AdminSideBar: React.FC = () => {
   ];
 
   return (
-    <>
-      {useUserStoreObj.permission === PermissionsTypes.ADMIN &&
-      useUserStoreObj.isLoggedIn ? (
-        <div className='flex h-screen w-full flex-col justify-center border-r-2 border-duoGray-light bg-duoGray-lighter font-extrabold tracking-wide text-duoGray-darker dark:border-duoGrayDark-light dark:bg-duoBlueDark-darkest dark:text-duoGrayDark-lightest'>
-          <label
-            className='mb-2 mt-2 flex items-center justify-center pb-2 pl-6 pr-6 pt-6
+    <div className='flex h-screen w-full flex-col justify-center border-r-2 border-duoGray-light bg-duoGray-lighter font-extrabold tracking-wide text-duoGray-darker dark:border-duoGrayDark-light dark:bg-duoBlueDark-darkest dark:text-duoGrayDark-lightest'>
+      <label
+        className='mb-2 mt-2 flex items-center justify-center pb-2 pl-6 pr-6 pt-6
            text-xl font-[850] text-duoBlue-default md:text-[1.5rem] lg:text-[2rem]'
-          >
-            duolingo
-          </label>
+      >
+        duolingo
+      </label>
 
-          <div className='flex items-center justify-center border-b-2 dark:border-duoGrayDark-light'>
-            <ul className='w-full cursor-default text-center text-[0.75rem] uppercase md:text-[0.9rem] lg:text-lg '>
-              {coursesList ? (
-                coursesList.length > 0 && !!coursesList[0]._id ? (
-                  // coursesList.map((item,index)=><></>)
-                  coursesList.map((item, index) => (
-                    <li
-                      key={item._id}
-                      className={`p-3
+      <div className='flex items-center justify-center border-b-2 dark:border-duoGrayDark-light'>
+        <ul className='w-full cursor-default text-center text-[0.75rem] uppercase md:text-[0.9rem] lg:text-lg '>
+          {coursesList ? (
+            coursesList.length > 0 && !!coursesList[0]._id ? (
+              // coursesList.map((item,index)=><></>)
+              coursesList.map((item, index) => (
+                <li
+                  key={item._id}
+                  className={`p-3
                        ${
                          item.name
                            ? pathname.includes(item.name.toLocaleLowerCase())
@@ -212,126 +199,118 @@ const AdminSideBar: React.FC = () => {
                              : ' text-duoGray-darkest hover:bg-duoBlue-lightest hover:text-duoBlue-light dark:text-duoGrayDark-lightest dark:hover:bg-sky-800 dark:hover:text-duoBlue-light'
                            : ' text-duoGray-darkest hover:bg-duoBlue-lightest hover:text-duoBlue-light dark:text-duoGrayDark-lightest dark:hover:bg-sky-800 dark:hover:text-duoBlue-light'
                        }`}
-                    >
-                      <Link
-                        href={`${
-                          item.name
-                            ? `/classroom/courses/${item.name.toLocaleLowerCase()}/students`
-                            : ''
-                        }`}
-                      >
-                        {item.name}
-                      </Link>
-                    </li>
-                  ))
-                ) : (
-                  <span className='flex items-center justify-center p-3 text-duoGray-darkest opacity-70 dark:text-duoGrayDark-lightest'>
-                    NO COURSES
-                  </span>
-                )
-              ) : (
-                <span className='flex items-center justify-center p-3 text-duoGray-darkest opacity-70 dark:text-duoGrayDark-lightest'>
-                  NO COURSES
-                </span>
-              )}
-            </ul>
-          </div>
-
-          <ul className='flex-grow'>
-            {sidebarItems.map((sideBaritem, index) => (
-              <li
-                key={sideBaritem.name}
-                onMouseEnter={() =>
-                  sideBaritem.subItems ? setIsHovered(true) : null
-                }
-                onMouseLeave={() => setIsHovered(false)}
-                ref={sidebarItemRef}
-                className={`duration-50 relative flex items-center justify-center transition md:justify-start ${
-                  //   selected === index
-                  //     ? 'cursor-pointer bg-duoBlue-lightest pb-3 pl-3 pr-3 pt-3 text-duoBlue-light'
-                  // :
-                  'cursor-pointer px-3 py-[0.85rem] hover:bg-duoGray-hover dark:hover:bg-duoBlueDark-dark 2xl:py-6'
-                }`}
-              >
-                <button
-                  className='mx-4 flex cursor-pointer flex-row items-center justify-center overflow-hidden'
-                  onClick={() =>
-                    sideBaritem.subItems
-                      ? null
-                      : sideBaritem.popup
-                        ? usePopupStoreObj.updateSelectedPopup(
-                            sideBaritem.popup
-                          )
-                        : null
-                  }
                 >
-                  {sideBaritem.href ? (
-                    <Link
-                      className='flex h-full cursor-pointer items-center justify-center overflow-hidden'
-                      href={sideBaritem.href}
-                    >
-                      {sideBaritem.icon ? (
-                        <FontAwesomeIcon
-                          className='fa-xs fa-solid h-7 w-7 2xl:h-8 2xl:w-8'
-                          icon={sideBaritem.icon}
-                        />
-                      ) : null}
+                  <Link
+                    href={`${
+                      item.name
+                        ? `/classroom/courses/${item.name.toLocaleLowerCase()}/students`
+                        : ''
+                    }`}
+                  >
+                    {item.name}
+                  </Link>
+                </li>
+              ))
+            ) : (
+              <span className='flex items-center justify-center p-3 text-duoGray-darkest opacity-70 dark:text-duoGrayDark-lightest'>
+                NO COURSES
+              </span>
+            )
+          ) : (
+            <span className='flex items-center justify-center p-3 text-duoGray-darkest opacity-70 dark:text-duoGrayDark-lightest'>
+              NO COURSES
+            </span>
+          )}
+        </ul>
+      </div>
+
+      <ul className='flex-grow'>
+        {sidebarItems.map((sideBaritem, index) => (
+          <li
+            key={sideBaritem.name}
+            onMouseEnter={() =>
+              sideBaritem.subItems ? setIsHovered(true) : null
+            }
+            onMouseLeave={() => setIsHovered(false)}
+            ref={sidebarItemRef}
+            className={`duration-50 relative flex items-center justify-center transition md:justify-start ${
+              //   selected === index
+              //     ? 'cursor-pointer bg-duoBlue-lightest pb-3 pl-3 pr-3 pt-3 text-duoBlue-light'
+              // :
+              'cursor-pointer px-3 py-[0.85rem] hover:bg-duoGray-hover dark:hover:bg-duoBlueDark-dark 2xl:py-6'
+            }`}
+          >
+            <button
+              className='mx-4 flex cursor-pointer flex-row items-center justify-center overflow-hidden'
+              onClick={() =>
+                sideBaritem.subItems
+                  ? null
+                  : sideBaritem.popup
+                    ? usePopupStoreObj.updateSelectedPopup(sideBaritem.popup)
+                    : null
+              }
+            >
+              {sideBaritem.href ? (
+                <Link
+                  className='flex h-full cursor-pointer items-center justify-center overflow-hidden'
+                  href={sideBaritem.href}
+                >
+                  {sideBaritem.icon ? (
+                    <FontAwesomeIcon
+                      className='fa-xs fa-solid h-7 w-7 2xl:h-8 2xl:w-8'
+                      icon={sideBaritem.icon}
+                    />
+                  ) : null}
+                  <p className='ml-4 hidden truncate text-lg md:block 2xl:text-xl'>
+                    {sideBaritem.name}
+                  </p>
+                </Link>
+              ) : (
+                <span className='overflow-hidden'>
+                  {sideBaritem.icon ? (
+                    <span className='flex w-full flex-row items-center justify-center'>
+                      <FontAwesomeIcon
+                        className='fa-xs fa-solid h-7 w-7 2xl:h-8 2xl:w-8'
+                        icon={sideBaritem.icon}
+                      />
                       <p className='ml-4 hidden truncate text-lg md:block 2xl:text-xl'>
                         {sideBaritem.name}
                       </p>
-                    </Link>
-                  ) : (
-                    <span className='overflow-hidden'>
-                      {sideBaritem.icon ? (
-                        <span className='flex w-full flex-row items-center justify-center'>
-                          <FontAwesomeIcon
-                            className='fa-xs fa-solid h-7 w-7 2xl:h-8 2xl:w-8'
-                            icon={sideBaritem.icon}
-                          />
-                          <p className='ml-4 hidden truncate text-lg md:block 2xl:text-xl'>
-                            {sideBaritem.name}
-                          </p>
-                        </span>
-                      ) : (
-                        <p className='truncate'>{sideBaritem.name}</p>
-                      )}
                     </span>
+                  ) : (
+                    <p className='truncate'>{sideBaritem.name}</p>
                   )}
-                </button>
-                {isHovered && sideBaritem.subItems ? (
-                  <ul className='absolute -top-[1rem] left-[90%] z-30 w-fit rounded-xl border-2 bg-duoGray-lighter py-3 dark:border-duoGrayDark-light dark:bg-duoBlueDark-darkest'>
-                    {sideBaritem.subItems.map((subItem, subItemIndex) => (
-                      <li
-                        className='duration-50 min-w-[10rem] py-2 pl-4 transition hover:bg-duoGray-light dark:hover:bg-duoBlueDark-default 2xl:py-3 2xl:pl-5 2xl:text-xl'
-                        key={subItem.name}
-                      >
-                        <button
-                          onClick={() =>
-                            subItem.popup
-                              ? usePopupStoreObj.updateSelectedPopup(
-                                  subItem.popup
-                                )
-                              : null
-                          }
-                        >
-                          {subItem.href ? (
-                            <Link href={subItem.href}>{subItem.name}</Link>
-                          ) : (
-                            <span>{subItem.name}</span>
-                          )}
-                        </button>
-                      </li>
-                    ))}
-                  </ul>
-                ) : null}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <></>
-      )}
-    </>
+                </span>
+              )}
+            </button>
+            {isHovered && sideBaritem.subItems ? (
+              <ul className='absolute -top-[1rem] left-[90%] z-30 w-fit rounded-xl border-2 bg-duoGray-lighter py-3 dark:border-duoGrayDark-light dark:bg-duoBlueDark-darkest'>
+                {sideBaritem.subItems.map((subItem, subItemIndex) => (
+                  <li
+                    className='duration-50 min-w-[10rem] py-2 pl-4 transition hover:bg-duoGray-light dark:hover:bg-duoBlueDark-default 2xl:py-3 2xl:pl-5 2xl:text-xl'
+                    key={subItem.name}
+                  >
+                    <button
+                      onClick={() =>
+                        subItem.popup
+                          ? usePopupStoreObj.updateSelectedPopup(subItem.popup)
+                          : null
+                      }
+                    >
+                      {subItem.href ? (
+                        <Link href={subItem.href}>{subItem.name}</Link>
+                      ) : (
+                        <span>{subItem.name}</span>
+                      )}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            ) : null}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,19 +3,13 @@ import { NextResponse, type NextRequest } from "next/server";
 import { PermissionsTypes } from "./app/store/stores/useUserStore";
 
 export async function middleware(request: NextRequest) {
+    const { pathname }: { pathname: string } = request.nextUrl;
+
     const jwtToken = request.cookies.get("jwtToken")?.value;
     console.log('middleware', jwtToken);
-    if (!!!jwtToken) {
 
-        return NextResponse.redirect(
-            new URL(
-                "/login",
-                request.url
-            )
-        );
-    }
     const userData = jwt.decode(
-        jwtToken,
+        jwtToken || '',
     ) as jwt.JwtPayload;
     console.log(userData)
     // return NextResponse.redirect(new URL('/login', request.url));
@@ -25,9 +19,55 @@ export async function middleware(request: NextRequest) {
             return NextResponse.redirect(new URL("/classroom", request.url));
         } else if (userData.role === PermissionsTypes.STUDENT) {
             return NextResponse.redirect(new URL("/learn", request.url));
+        } else {
+            return NextResponse.redirect(
+                new URL(
+                    "/login",
+                    request.url
+                )
+            );
         }
     };
-    Redirect();
+    if (!!jwtToken && pathname === 'login') {
+        return Redirect();
+    }
+
+
+    if (
+        (!!jwtToken && pathname.startsWith("/classroom") && userData.role !== PermissionsTypes.ADMIN) ||
+
+        (!!jwtToken &&
+            pathname.startsWith("/learn") || pathname.startsWith("/lesson") &&
+            userData.role !== PermissionsTypes.STUDENT)
+    ) {
+        return Redirect();
+    }
+
+    if (!jwtToken) {
+        if (
+            pathname.includes("/classroom") ||
+            pathname.includes("/learn") ||
+            pathname.includes("/lesson")
+        ) {
+            return Response.json(
+                { success: false, message: "authentication failed" },
+                { status: 401 }
+            );
+        }
+    } else {
+        if (
+            (!!jwtToken && pathname.startsWith("/classroom") && userData.role !== PermissionsTypes.ADMIN) ||
+            (!!jwtToken &&
+                pathname.startsWith("/learn") || pathname.startsWith("/lesson") &&
+                userData.role !== PermissionsTypes.STUDENT)
+        ) {
+            console.log(pathname, userData.role);
+            return Response.json(
+                { success: false, message: "authentication failed" },
+                { status: 401 }
+            );
+        }
+    }
 
 }
 
