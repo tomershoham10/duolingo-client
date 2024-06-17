25b840df9aaacade9ce238b1c8da4e0cf071a535
tomershoham10 <tomershoham10@gmail.com>
2024-06-17 11:25:10 +0300

    refactor: changed svg path

--- src/components/AudioPlayer/page.tsx
+++ src/components/AudioPlayer/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Image from 'next/image';
 import { useRef, useState } from 'react';
-import audioIcon from './svgs/animated_sound_wave.svg';
+import audioIcon from '../../../public/svgs/audioPlayer/playing.svg';
 
 const AudioPlayer: React.FC<AudioPlayerProps> = (props) => {
   const src = props.src;
@@ -28,7 +28,7 @@ const AudioPlayer: React.FC<AudioPlayerProps> = (props) => {
 
   return (
     <section
-      className={`flex h-[12.5rem] w-[12.5rem] cursor-pointer flex-col justify-end rounded-[25%] border-b-[6px] active:dark:bg-transparent  lg:h-[20rem] lg:w-[20rem] 2xl:h-[30rem] 2xl:w-[30rem] 3xl:h-[35rem] 3xl:w-[35rem] ${
+      className={`flex h-[12.5rem] w-[12.5rem] cursor-pointer flex-col justify-end rounded-[25%] border-b-[8px] active:dark:bg-transparent  lg:h-[20rem] lg:w-[20rem] 2xl:h-[30rem] 2xl:w-[30rem] 3xl:h-[35rem] 3xl:w-[35rem] ${
         isPlaying
           ? 'hover:dark:border[#2E75A0] border-[#3383B1] dark:bg-duoBlueDark-text hover:dark:bg-duoBlueDark-textHover'
           : 'border-[#5F666F] dark:bg-duoGrayDark-lightestOpacity hover:dark:border-[#565D65] hover:dark:bg-[#7F8D96]'
@@ -44,8 +44,8 @@ const AudioPlayer: React.FC<AudioPlayerProps> = (props) => {
       <button
         className={`flex h-full w-full items-center justify-center rounded-[25%] ${
           isPlaying
-            ? 'active:translate-y-[6px] dark:bg-duoBlueDark-text hover:dark:bg-duoBlueDark-textHover'
-            : 'active:translate-y-[6px] dark:bg-duoGrayDark-lightestOpacity hover:dark:bg-[#7F8D96]'
+            ? 'active:translate-y-[8px] dark:bg-duoBlueDark-text hover:dark:bg-duoBlueDark-textHover'
+            : 'active:translate-y-[8px] dark:bg-duoGrayDark-lightestOpacity hover:dark:bg-[#7F8D96]'
         }`}
         onClick={togglePlayPause}
       >
