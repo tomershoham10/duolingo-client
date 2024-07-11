0b1c11c4971854a66333869be921d2d74e4c170d
tomershoham10 <tomershoham10@gmail.com>
2024-07-11 12:51:19 +0300

    feat: metadata validation fuctions

--- src/app/utils/functions/filesMetadata/functions.ts
+++ src/app/utils/functions/filesMetadata/functions.ts
@@ -1,3 +1,13 @@
-export const isFSAMetadata = (metadata: Partial<Metadata>): metadata is Partial<FSAMetadata> => {
-    return (metadata as FSAMetadata).targets_ids_list !== undefined;
+import { ExercisesTypes } from "@/app/API/classes-service/exercises/functions";
+
+export const isFSAMetadata = (metadata: Partial<Metadata> | undefined): metadata is Partial<FSAMetadata> => {
+    return metadata !== undefined && (metadata as Partial<FSAMetadata>).record_length !== undefined;
+}
+
+export const isRecordMetadata = (metadata: Partial<Metadata> | undefined): metadata is Partial<RecordMetadata> => {
+    return metadata !== undefined && (metadata as Partial<RecordMetadata>).record_length !== undefined;
+}
+
+export const isSonogramMetadata = (metadata: Partial<Metadata> | undefined, exerciseType: ExercisesTypes): metadata is Partial<SonogramMetadata> => {
+    return metadata !== undefined && exerciseType === ExercisesTypes.FSA;
 }
