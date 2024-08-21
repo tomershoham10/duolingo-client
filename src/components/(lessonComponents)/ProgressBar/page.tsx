ca756852a305c586f33b5b6d444d81d6b7498a25
tomershoham10 <tomershoham10@gmail.com>
2024-08-21 17:33:23 +0300

    refactor: changed lesson page to sub components

---
+++ src/components/(lessonComponents)/ProgressBar/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+const ProgressBar: React.FC<ProgressBarProps> = ({
+  totalNumOfExercises,
+  numOfExercisesMade,
+}) => {
+  const [newwidth, setNewwidth] = useState<string>('0');
+
+  useEffect(() => {
+    const width = ((100 * numOfExercisesMade) / totalNumOfExercises).toString();
+    setNewwidth(width);
+  }, [totalNumOfExercises, numOfExercisesMade]);
+
+  return (
+    <div className='w-full px-10 py-5'>
+      {totalNumOfExercises > 0 ? (
+        <div className='h-4 w-full rounded-2xl bg-duoGray-default text-transparent dark:bg-duoGrayDark-light'>
+          <div
+            className='dark:bg-duoGreenDark-default relative flex h-full items-center justify-start rounded-2xl bg-duoGreen-default'
+            style={{
+              width: `${newwidth}%`,
+              transition: 'width 0.5s ease-in-out 0.5s',
+            }}
+          >
+            <div className='mx-[10%] mb-[2.5px] w-[80%] sm:mx-[4%] sm:w-[92%] md:mx-2 md:w-full'>
+              <div className='dark:bg-duoGreenDark-light max-h-[5px] w-full rounded-3xl bg-duoGreen-light'>
+                {' '}
+                {newwidth}
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+export default ProgressBar;
---
+++ src/components/(lessonComponents)/lessonFooter/page.tsx
@@ -0,0 +1,9 @@
+const LessonFooter = () => {
+  return (
+    <section className='relative col-span-2 flex items-center justify-center border-t-2 dark:border-duoGrayDark-light'>
+      footer
+    </section>
+  );
+};
+
+export default LessonFooter;
