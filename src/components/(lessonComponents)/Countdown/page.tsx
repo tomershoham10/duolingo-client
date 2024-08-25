e5c247d5055bb14bfac7aa8ed64f9c46e22f7307
tomershoham10 <tomershoham10@gmail.com>
2024-08-25 09:46:54 +0300

    feat: countdown component

---
+++ src/components/(lessonComponents)/Countdown/page.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+interface CountdownProps {
+  onComplete: () => void;
+}
+
+const Countdown: React.FC<CountdownProps> = ({ onComplete }) => {
+  const [count, setCount] = useState(3);
+
+  useEffect(() => {
+    if (count > 0) {
+      const timer = setTimeout(() => setCount(count - 1), 1000);
+      return () => clearTimeout(timer);
+    } else {
+      onComplete(); // Trigger the callback when the countdown ends
+    }
+  }, [count, onComplete]);
+
+  return (
+    <div className='text-center text-7xl font-extrabold'>
+      {count > 0 && count}
+    </div>
+  );
+};
+
+export default Countdown;
