9c462bd37995659024d8d008a13a82e281728445
tomershoham10 <tomershoham10@gmail.com>
2024-08-25 09:47:47 +0300

    refactor: deleted unused code + now using props as an object

--- src/app/(popups)/AdminEditPopup/EditLesson/page.tsx
+++ src/app/(popups)/AdminEditPopup/EditLesson/page.tsx
@@ -9,12 +9,10 @@ import {
   editLessonReducer,
   editLessonType,
 } from '@/reducers/adminView/popups/editLessonReducer';
-import Table, { TableRow } from '@/components/Table/page';
-import { getFileMetadataByName } from '@/app/API/files-service/functions';
+import Table from '@/components/Table/page';
 import Button, { ButtonColors, ButtonTypes } from '@/components/Button/page';
 import { updateLesson } from '@/app/API/classes-service/lessons/functions';
 import pRetry from 'p-retry';
-import { isFSAMetadata } from '@/app/_utils/functions/filesMetadata/functions';
 
 library.add(faXmark);
 
@@ -27,13 +25,9 @@ interface EditLessonProps {
 }
 
 const EditLesson: React.FC<EditLessonProps> = (props) => {
+  const { lessonId, lessonIndex, exercisesList, onClose } = props;
+  console.log('EditLesson popup', exercisesList);
   const headers = [
-    // { key: 'answersList', label: 'target name' },
-    // { key: 'difficultyLevel', label: 'difficulty level' },
-    // { key: 'isInItalkia', label: 'is in italkia' },
-    // { key: 'signature_type', label: 'signature type' },
-    // { key: 'isInLessons', label: 'is in other lessons' },
-    // { key: 'sonolist', label: 'sonolist' },
     { key: 'type', label: 'exercise type' },
     { key: 'dateCreated', label: 'date created' },
   ];
@@ -52,8 +46,6 @@ const EditLesson: React.FC<EditLessonProps> = (props) => {
 
   useEffect(() => {
     const fetchExercises = async () => {
-      //   const res = await getAllExercises();
-
       const res = await pRetry(getAllExercises, {
         retries: 5,
       });
@@ -70,75 +62,17 @@ const EditLesson: React.FC<EditLessonProps> = (props) => {
     fetchExercises();
   }, []);
 
-  //   useEffect(() => {
-  //     const fetchRecords = async () => {
-  //       const fetchPromises = editLessonState.exercisesList.map(
-  //         async (exercise) => {
-  //           try {
-  //             if(exercise.type===ExercisesTypes.FSA){
-
-  //             const exerciseRecName = exercise.files[0].fileName;
-  //             console.log('exercise loop - exerciseRecName', exerciseRecName);
-
-  //             const recData = await pRetry(
-  //               () =>
-  //                 getFileMetadataByName(
-  //                   BucketsNames.RECORDS,
-  //                   ExercisesTypes.FSA,
-  //                   exerciseRecName
-  //                 ),
-  //               {
-  //                 retries: 5,
-  //               }
-  //             );
-  //             console.log('recData', recData);
-  //             if (!recData) {
-  //               return null;
-  //             }
-  //             if (isFSAMetadata(recData.metadata)) {
-  //               const newRow = {
-  //                 _id: exercise._id,
-  //                 targetsList: exercise.targetsList,
-  //                 difficultyLevel: recData.metadata.difficulty_level,
-  //                 is_in_italy: recData.metadata.is_in_italy,
-  //                 signature_type: recData.metadata.signature_type,
-  //                 sonolist: recData.metadata.sonograms_names,
-  //               };
-  //               console.log('newRow', newRow);
-  //               return newRow;
-  //             }
-  //           } catch (error) {
-  //             console.error('Error fetching record:', error);
-  //             return null;
-  //           }
-  //         }
-  //       );
-  //       const tableRows = await Promise.all(fetchPromises);
-  //       const filteredRows = tableRows.filter(
-  //         (row) => row !== null
-  //       ) as TableRow[];
-  //       editLessonDispatch({
-  //         type: editLessonAction.SET_TABLE_DATA,
-  //         payload: filteredRows,
-  //       });
-  //     };
-  //     if (editLessonState.exercisesList.length > 0) {
-  //       fetchRecords();
-  //     }
-  //   }, [editLessonState.exercisesList]);
-
   useEffect(() => {
     console.log('editLessonState.tableData', editLessonState.tableData);
   }, [editLessonState.tableData]);
 
   const addExerciseToLesson = async () => {
-    // if (!!editLessonState.selectedFSA) {
     const status = await pRetry(
       () =>
         !!editLessonState.selectedExercise
-          ? updateLesson(props.lessonId, {
+          ? updateLesson(lessonId, {
               exercisesIds: [
-                ...props.exercisesList,
+                ...exercisesList,
                 editLessonState.selectedExercise,
               ],
             })
@@ -147,24 +81,20 @@ const EditLesson: React.FC<EditLessonProps> = (props) => {
         retries: 5,
       }
     );
-    //   const status = await updateLesson(props.lessonId, {
-    //     exercises: [...props.exercisesList, editLessonState.selectedFSA],
-    //   });
     alert(status);
-    // }
   };
 
   return (
     <div className='relative m-5 flex h-[30rem] w-[40rem] rounded-md bg-white p-5 dark:bg-duoGrayDark-darkest xl:h-[35rem] xl:w-[55rem] 2xl:h-[50rem] 2xl:w-[78.5rem] 3xl:h-[70rem] 3xl:w-[110rem]'>
       <button
-        onClick={props.onClose}
+        onClick={onClose}
         className='absolute z-50 h-fit w-fit flex-none rounded-md text-duoGray-dark'
       >
         <FontAwesomeIcon className='fa-lg fa-solid flex-none' icon={faXmark} />
       </button>
       <div className='absolute left-0 flex h-10 w-full justify-center border-b-2 dark:border-duoBlueDark-text'>
         <span className='text-xl font-extrabold tracking-widest text-duoGray-dark dark:text-duoBlueDark-text'>
-          lesson #{props.lessonIndex + 1}
+          lesson #{lessonIndex + 1}
         </span>
       </div>
       <div className='mx-auto mt-16 h-[25rem] w-[90%]'>
