06562cfe26b99e1adbcd036778bfa6d7e1edabb3
tomershoham10 <tomershoham10@gmail.com>
2024-08-25 09:46:09 +0300

    feat: enabling spotrecc sub exercises in lesson page

--- src/app/lesson/(exercises)/_spotrecc/page.tsx
+++ src/app/lesson/(exercises)/_spotrecc/page.tsx
@@ -1,60 +1,167 @@
+import { useCallback, useEffect, useState } from 'react';
+import Image from 'next/image';
+import pRetry from 'p-retry';
+
 import { ExercisesTypes } from '@/app/API/classes-service/exercises/functions';
 import { BucketsNames, getFileByName } from '@/app/API/files-service/functions';
+import Countdown from '@/components/(lessonComponents)/Countdown/page';
 import AudioPlayer, { AudioPlayerSizes } from '@/components/AudioPlayer/page';
-import pRetry from 'p-retry';
-import { useCallback, useEffect, useState } from 'react';
 
 interface SpotreccPageProps {
   exercise: SpotreccType;
+  isExerciseStarted: boolean;
+  updateTimer: (time: number) => void;
+  //   finishSubExercise: () => void;
+  finishMainExercise: () => void;
+  currentSubExerciseIndex: number;
 }
-
 const SpotreccPage: React.FC<SpotreccPageProps> = (props) => {
-  const [currentSubExerciseIndex, setCurrentSubExerciseIndex] =
-    useState<number>(0);
-  const { exercise } = props;
+  const {
+    exercise,
+    isExerciseStarted,
+    updateTimer,
+    // finishSubExercise,
+    finishMainExercise,
+    currentSubExerciseIndex,
+  } = props;
+
   const currentSubExercise = exercise.subExercises[currentSubExerciseIndex];
 
   const [url, setUrl] = useState<string | null>(null);
 
-  const getFile = useCallback(async (fileName: string) => {
-    const responseUrl = await pRetry(
-      () =>
-        fileName.endsWith('.wav')
-          ? getFileByName(
-              BucketsNames.RECORDS,
-              ExercisesTypes.SPOTRECC,
-              fileName
-            )
-          : getFileByName(
-              BucketsNames.IMAGES,
-              ExercisesTypes.SPOTRECC,
-              fileName
-            ),
-      {
-        retries: 5,
-      }
-    );
-    setUrl(responseUrl);
+  const [showCountdown, setShowCountdown] = useState<boolean>(false);
+
+  const [timeLeft, setTimeLeft] = useState<number>(currentSubExercise.time);
+
+  const [isAudioPlaying, setIsAudioPlaying] = useState<boolean>(false);
+
+  const handleCountdownComplete = useCallback(() => {
+    setShowCountdown(false);
   }, []);
 
   useEffect(() => {
+    if (isExerciseStarted) {
+      setShowCountdown(true);
+    }
+  }, [handleCountdownComplete, isExerciseStarted]);
+
+  const setWavLength = useCallback(async (responseUrl: string) => {
+    const audio = new Audio(responseUrl);
+    length = await new Promise((resolve) => {
+      audio.addEventListener('loadedmetadata', () => {
+        resolve(audio.duration);
+      });
+    });
+    console.log('setWavLength', length);
+    setTimeLeft(length);
+  }, []);
+
+  const getFile = useCallback(
+    async (fileName: string) => {
+      const responseUrl = await pRetry(
+        () =>
+          fileName.endsWith('.wav')
+            ? getFileByName(
+                BucketsNames.RECORDS,
+                ExercisesTypes.SPOTRECC,
+                fileName
+              )
+            : getFileByName(
+                BucketsNames.IMAGES,
+                ExercisesTypes.SPOTRECC,
+                fileName
+              ),
+        {
+          retries: 5,
+        }
+      );
+      setUrl(responseUrl);
+      fileName.endsWith('.wav') && responseUrl && setWavLength(responseUrl);
+    },
+    [setWavLength]
+  );
+
+  useEffect(() => {
+    setUrl(null);
     getFile(currentSubExercise.fileName);
+    !currentSubExercise.fileName.endsWith('.wav') &&
+      setTimeLeft(currentSubExercise.time);
   }, [currentSubExercise, getFile]);
 
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+
+    if (
+      isExerciseStarted &&
+      !showCountdown &&
+      (currentSubExercise.fileName.endsWith('.wav') ? isAudioPlaying : true)
+    ) {
+      interval = setInterval(() => {
+        setTimeLeft((prevTime) => {
+          const newTime = prevTime > 0 ? prevTime - 1 : 0;
+          return newTime;
+        });
+      }, 1000);
+    }
+
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [
+    currentSubExercise.fileName,
+    currentSubExerciseIndex,
+    exercise.subExercises.length,
+    finishMainExercise,
+    isAudioPlaying,
+    isExerciseStarted,
+    showCountdown,
+  ]);
+
+  useEffect(() => {
+    if (timeLeft >= 0) {
+      updateTimer(timeLeft);
+    } else if (
+      timeLeft === 0 &&
+      currentSubExerciseIndex === exercise.subExercises.length - 1
+    ) {
+      finishMainExercise();
+    }
+  }, [
+    timeLeft,
+    updateTimer,
+    currentSubExerciseIndex,
+    exercise.subExercises.length,
+    finishMainExercise,
+  ]);
+
   return (
-    <section className='flex h-full flex-col items-center justify-center'>
-      {url ? (
-        currentSubExercise.fileName.endsWith('.wav') ? (
-          <AudioPlayer
-            src={url}
-            size={AudioPlayerSizes.MEDIUM}
-            isPauseable={true}
-          />
+    <section className='flex flex-col items-center justify-center'>
+      {isExerciseStarted ? (
+        url ? (
+          showCountdown ? (
+            <Countdown onComplete={handleCountdownComplete} />
+          ) : currentSubExercise.fileName.endsWith('.wav') ? (
+            <AudioPlayer
+              src={url}
+              size={AudioPlayerSizes.MEDIUM}
+              isPauseable={false}
+              isDisabled={!isExerciseStarted}
+              onPlay={() => setIsAudioPlaying(true)}
+            />
+          ) : (
+            <>
+              {timeLeft > 0 ? (
+                <Image src={url} alt='spotrecc img' width={500} height={300} />
+              ) : (
+                <p>selecet a target</p>
+              )}
+            </>
+          )
         ) : (
-          <p>image</p>
+          <p>loading...</p>
         )
       ) : (
-        <p>loading...</p>
+        <p>SPOTRECC</p>
       )}
       {/* <button
         onClick={() =>
@@ -62,7 +169,7 @@ const SpotreccPage: React.FC<SpotreccPageProps> = (props) => {
             return prev + 1 < exercise.subExercises.length ? prev + 1 : prev;
           })
         }
-      >
+      > 
         next
       </button> */}
     </section>
--- src/app/lesson/page.tsx
+++ src/app/lesson/page.tsx
@@ -10,15 +10,36 @@ import { ExercisesTypes } from '../API/classes-service/exercises/functions';
 import SpotreccPage from './(exercises)/_spotrecc/page';
 import LessonInfo from '@/components/InfoBar/LessonInfo/page';
 import LessonFooter from '@/components/(lessonComponents)/lessonFooter/page';
+import { TimeType } from '@/reducers/studentView/(exercises)/fsaReducer';
+import { formatNumberToMinutes } from '../_utils/functions/formatNumberToMinutes';
 
 const Lesson: React.FC = () => {
   const nextLessonId = useStore(useUserStore, (state) => state.nextLessonId);
   const userId = useStore(useUserStore, (state) => state.userId);
   const infoBarRef = useRef<HTMLDivElement | null>(null);
-
-  const [currentExercise, setCurrentExercise] = useState<
-    FsaType | SpotreccType | null
+  const [exercisesData, setExercisesData] = useState<
+    (FsaType | SpotreccType)[] | null
   >(null);
+  const [currentExerciseIndex, setCurrentExerciseIndex] = useState<number>(0);
+
+  const [currentSpotreccSubIndex, setCurrentSpotreccSubIndex] =
+    useState<number>(0);
+
+  const [isExerciseStarted, setIsExerciseStarted] = useState<boolean>(false);
+  const [isExerciseFinished, setIsExerciseFinished] = useState<boolean>(false);
+
+  const [timeRemaining, setTimeRemaining] = useState<TimeType>({
+    minutes: 0,
+    seconds: 0,
+  });
+
+  const currentExercise = exercisesData
+    ? exercisesData[currentExerciseIndex]
+    : null;
+
+  useEffect(() => {
+    console.log('currentExercise', currentExercise);
+  }, [currentExercise]);
 
   const fetchData = useCallback(async () => {
     const response = await pRetry(
@@ -29,17 +50,45 @@ const Lesson: React.FC = () => {
     );
     console.log('SET_EXERCISES_DATA', response);
     if (response) {
-      setCurrentExercise(response.exercises[0]);
+      setExercisesData(response.exercises);
     }
   }, [nextLessonId]);
 
   useEffect(() => {
-    console.log('currentExercise', currentExercise);
-  }, [currentExercise]);
-
-  useEffect(() => {
     fetchData();
   }, [fetchData, nextLessonId]);
+
+  const handleExerciseStarted = useCallback(() => {
+    setIsExerciseStarted(true);
+  }, []);
+
+  const updateTimer = useCallback((time: number) => {
+    console.log('handleSpotrecc', time);
+    if (time > 60) {
+      const minues= formatNumberToMinutes(time);
+      setTimeRemaining({ minutes: 0, seconds: time });
+    }
+  }, []);
+
+  const finishMainExercise = useCallback(() => {
+    console.log('finish');
+  }, []);
+
+  const handleSubmit = useCallback(() => {
+    if (currentExercise) {
+      if (currentExercise.type === ExercisesTypes.FSA) {
+      }
+      if (currentExercise.type === ExercisesTypes.SPOTRECC) {
+        const spotreccExer = currentExercise as SpotreccType;
+        console.log(currentSpotreccSubIndex, spotreccExer.subExercises.length);
+        if (currentSpotreccSubIndex < spotreccExer.subExercises.length - 1) {
+          setCurrentSpotreccSubIndex((prev) => prev + 1);
+        } else {
+          console.log('finish');
+        }
+      }
+    }
+  }, [currentExercise, currentSpotreccSubIndex]);
 
   return (
     <section
@@ -47,12 +96,22 @@ const Lesson: React.FC = () => {
       id='lesson-grid'
     >
       <section className='h-full' id='exercise-section'>
-        <ProgressBar totalNumOfExercises={5} numOfExercisesMade={2} />
-        {currentExercise &&
+        <ProgressBar
+          totalNumOfExercises={exercisesData?.length || 1}
+          numOfExercisesMade={currentExerciseIndex}
+        />
+        {exercisesData &&
+          currentExercise &&
           (currentExercise.type === ExercisesTypes.FSA ? (
             <>{ExercisesTypes.FSA}</>
           ) : currentExercise.type === ExercisesTypes.SPOTRECC ? (
-            <SpotreccPage exercise={currentExercise as SpotreccType} />
+            <SpotreccPage
+              exercise={currentExercise as SpotreccType}
+              isExerciseStarted={isExerciseStarted}
+              updateTimer={updateTimer}
+              finishMainExercise={finishMainExercise}
+              currentSubExerciseIndex={currentSpotreccSubIndex}
+            />
           ) : (
             <>error</>
           ))}
@@ -61,14 +120,26 @@ const Lesson: React.FC = () => {
         <LessonInfo
           infoRef={infoBarRef}
           exerciseType={currentExercise?.type || ExercisesTypes.FSA}
-          timeRemaining={{ minutes: 0, seconds: 0 }}
-          isExerciseStarted={false}
-          isExerciseFinished={false}
+          timeRemaining={timeRemaining}
+          isExerciseStarted={isExerciseStarted}
+          isExerciseFinished={isExerciseFinished}
           targetsToSubmit={[]}
         />
       </section>
       <section id='footer-section'>
-        <LessonFooter />
+        <LessonFooter
+          exerciseType={currentExercise?.type || ExercisesTypes.SPOTRECC}
+          fileName={
+            currentExercise?.type === ExercisesTypes.FSA
+              ? (currentExercise as FsaType).fileName
+              : undefined
+          }
+          isExerciseStarted={isExerciseStarted}
+          isExerciseFinished={isExerciseFinished}
+          onStartExercise={() => handleExerciseStarted()}
+          onSubmit={handleSubmit}
+          updatePassword={(password) => console.log('zip', password)}
+        />
       </section>
     </section>
   );
