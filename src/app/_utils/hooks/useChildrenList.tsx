c40e8c481ad9a9447eba74afa4f075ccde9d155c
tomershoham10 <tomershoham10@gmail.com>
2024-08-28 13:17:03 +0300

    feat: costum hook to get the children list (targets objects) of a targetId

---
+++ src/app/_utils/hooks/useChildrenList.tsx
@@ -0,0 +1,26 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { useTargetStore } from '@/app/store/stores/(dropdowns)/useTargetStore';
+
+export const useChildrenList = (targetId: string) => {
+  const [children, setChildren] = useState<TargetType[]>([]);
+  const targets = useTargetStore((state) => state.targets);
+
+  useEffect(() => {
+    const getChildrenList = (
+      targets: TargetType[],
+      targetId: string
+    ): TargetType[] => {
+      const target = targets.find((t) => t._id === targetId);
+      if (target) {
+        return targets.filter((t) => target.children.includes(t._id));
+      }
+      return [];
+    };
+
+    const childrenList = getChildrenList(targets, targetId);
+    setChildren(childrenList);
+  }, [targets, targetId]);
+
+  return children;
+};
