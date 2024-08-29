33ec994c9583478487a7ab8734798282c42561d7
tomershoham10 <tomershoham10@gmail.com>
2024-08-29 16:11:18 +0300

    feat: countries + orgs stores and costume hooks

--- src/components/TargetsDropdowns/index.tsx
+++ src/components/TargetsDropdowns/index.tsx
@@ -1,61 +1,55 @@
 'use client';
+import { useFetchCountries } from '@/app/_utils/hooks/(dropdowns)/useFechCountries';
+import { useFetchOrganizations } from '@/app/_utils/hooks/(dropdowns)/useFechOrganizations';
+import { useFetchTargets } from '@/app/_utils/hooks/(dropdowns)/useFechTargets';
 import { useChildrenList } from '@/app/_utils/hooks/useChildrenList';
 import Dropdown, { DropdownSizes } from '@/components/Dropdown';
 import { useCallback, useMemo, useState } from 'react';
 
-const TargetsDropdowns: React.FC = () => {
+interface TargetsDropdownsProps {
+  excludeFileType?: boolean;
+  onModelSelected?: (model: TargetType) => void;
+}
+
+const TargetsDropdowns: React.FC<TargetsDropdownsProps> = (props) => {
+  const { excludeFileType, onModelSelected } = props;
   const [selctedCountry, setSelctedCountry] = useState<CountryType | null>(
     null
   );
+  const [selectedMainType, setSelectedMainType] = useState<TargetType | null>(
+    null
+  );
+  const [selectedSubType, setSelectedSubType] = useState<TargetType | null>(
+    null
+  );
+  const [selectedModel, setSelectedModel] = useState<TargetType | null>(null);
 
   const [selctedOrganization, setSelctedOrganization] =
     useState<OrganizationType | null>(null);
 
-  const countries = useMemo(() => {
-    return [
-      {
-        _id: '111',
-        country_name: 'Israel',
-      },
-      {
-        _id: '122',
-        country_name: 'U.S.A.',
-      },
-    ] as unknown as CountryType[];
-  }, []);
-
-  const organizations = useMemo(() => {
-    return [
-      {
-        _id: 211,
-        organization_name: 'Israeli Navy',
-        country: '111',
-      },
-      {
-        _id: 222,
-        organization_name: 'U.S.N.',
-        country: '122',
-      },
-    ] as unknown as OrganizationType[];
-  }, []);
+  const targetsList = useFetchTargets();
+  const countiresList = useFetchCountries();
+  const organizationsList = useFetchOrganizations();
+  console.log('TargetsDropdowns', targetsList);
 
   const handleCountryChange = useCallback(
     (countryName: string) => {
-      const countryObj = countries.find(
+      const countryObj = countiresList.find(
         (country) => country.country_name === countryName
       );
 
       setSelctedCountry(countryObj || null);
       setSelctedOrganization(null);
     },
-    [countries]
+    [countiresList]
   );
 
   const filteredOrganizationsList = useMemo(() => {
-    if (!selctedCountry)
-      return organizations.map((org) => org.organization_name); // Return an empty array if no country is selected
+    if (!selctedCountry) {
+      return organizationsList.map((org) => org.organization_name);
+    }
     console.log(selctedCountry._id);
-    const filteredOrganizations = organizations.filter(
+    const filteredOrganizations = organizationsList.filter(
       (organization) => organization.country === selctedCountry._id
     );
     console.log(filteredOrganizations);
@@ -65,16 +59,93 @@ const TargetsDropdowns: React.FC = () => {
     );
     console.log(filteredOrganizationsNames);
     return filteredOrganizationsNames;
-  }, [selctedCountry, organizations]);
+  }, [selctedCountry, organizationsList]);
+
+  const filteredSubTypesList = useMemo(() => {
+    if (!selectedMainType) {
+      return targetsList
+        .filter((target) => target.level === 2)
+        .map((target) => target.name);
+    }
+
+    const filteredSubTypes = targetsList.filter(
+      (target) => target.level === 2 && target.father === selectedMainType._id
+    );
+    const filteredSubTypesNames = filteredSubTypes.map(
+      (subType) => subType.name
+    );
+
+    return filteredSubTypesNames;
+  }, [selectedMainType, targetsList]);
+
+  const filteredModelsList = useMemo(() => {
+    if (!selectedSubType) {
+      return [];
+    }
+
+    const filteredModels = targetsList.filter(
+      (target) =>
+        target.level === 3 &&
+        target.father === selectedSubType._id &&
+        (selctedOrganization
+          ? target.organization?.includes(selctedOrganization._id)
+          : true)
+    );
+    const filteredModelsNames = filteredModels.map((model) => model.name);
+
+    return filteredModelsNames;
+  }, [selctedOrganization, selectedSubType, targetsList]);
+
   const handleOrganizationChange = useCallback(
     (organizationName: string) => {
-      const organizationObj = organizations.find(
+      const organizationObj = organizationsList.find(
         (organization) => organization.organization_name === organizationName
       );
 
       setSelctedOrganization(organizationObj || null);
+      setSelectedModel(null);
     },
-    [organizations]
+    [organizationsList]
+  );
+
+  const handelMainTypeChange = useCallback(
+    (targetName: string) => {
+      const filtedMain = targetsList.find(
+        (target) => target.name === targetName && target.level === 1
+      );
+      setSelectedMainType(filtedMain || null);
+      setSelectedSubType(null);
+      setSelectedModel(null);
+    },
+    [targetsList]
+  );
+
+  const handelSubTypeChange = useCallback(
+    (targetName: string) => {
+      const filtedSubType = targetsList.find(
+        (target) => target.name === targetName && target.level === 2
+      );
+      setSelectedSubType(filtedSubType || null);
+      setSelectedModel(null);
+    },
+    [targetsList]
+  );
+
+  const handelModelChange = useCallback(
+    (targetName: string) => {
+      const filtedModel = targetsList.find(
+        (target) =>
+          target.name === targetName &&
+          target.level === 3 &&
+          target.father === selectedSubType?._id &&
+          (selctedOrganization
+            ? target.organization?.includes(selctedOrganization._id)
+            : true)
+      );
+      setSelectedModel(filtedModel || null);
+      onModelSelected && filtedModel && onModelSelected(filtedModel);
+    },
+    [onModelSelected, selctedOrganization, selectedSubType?._id, targetsList]
   );
 
   return (
@@ -84,7 +155,7 @@ const TargetsDropdowns: React.FC = () => {
           isSearchable={true}
           placeholder={'Country'}
           value={selctedCountry?.country_name}
-          items={countries.map((country) => country.country_name)}
+          items={countiresList.map((country) => country.country_name)}
           onChange={handleCountryChange}
           size={DropdownSizes.SMALL}
         />
@@ -103,9 +174,11 @@ const TargetsDropdowns: React.FC = () => {
         <Dropdown
           isSearchable={true}
           placeholder={'Main type'}
-          items={[]}
-          //   onChange={(target: TargetType) => handleTargetChange(target)}
-          onChange={() => {}}
+          value={selectedMainType?.name}
+          items={targetsList
+            .filter((target) => target.level === 1)
+            .map((target) => target.name)}
+          onChange={handelMainTypeChange}
           size={DropdownSizes.SMALL}
         />
       </section>
@@ -113,9 +186,9 @@ const TargetsDropdowns: React.FC = () => {
         <Dropdown
           isSearchable={true}
           placeholder={'Sub type'}
-          items={[]}
-          //   onChange={(target: TargetType) => handleTargetChange(target)}
-          onChange={() => {}}
+          value={selectedSubType?.name}
+          items={filteredSubTypesList}
+          onChange={handelSubTypeChange}
           size={DropdownSizes.SMALL}
         />
       </section>
@@ -123,21 +196,23 @@ const TargetsDropdowns: React.FC = () => {
         <Dropdown
           isSearchable={true}
           placeholder={'Model'}
-          items={[]}
-          //   onChange={(target: TargetType) => handleTargetChange(target)}
-          onChange={() => {}}
+          value={selectedModel?.name}
+          items={filteredModelsList}
+          onChange={handelModelChange}
           size={DropdownSizes.SMALL}
         />
       </section>
-      <section className='w-[15rem]'>
-        <Dropdown
-          isSearchable={true}
-          placeholder={'File type'}
-          items={[]}
-          onChange={() => {}}
-          size={DropdownSizes.SMALL}
-        />
-      </section>
+      {!excludeFileType && (
+        <section className='w-[15rem]'>
+          <Dropdown
+            isSearchable={true}
+            placeholder={'File type'}
+            items={[]}
+            onChange={() => {}}
+            size={DropdownSizes.SMALL}
+          />
+        </section>
+      )}
     </section>
   );
 };
