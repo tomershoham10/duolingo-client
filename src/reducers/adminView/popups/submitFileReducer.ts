84d268f1144e6ec4c4aab8be291c1c47b876d92a
tomershoham10 <tomershoham10@gmail.com>
2024-08-29 16:10:40 +0300

    refactor: adjustments to target type change

--- src/reducers/adminView/popups/submitFileReducer.ts
+++ src/reducers/adminView/popups/submitFileReducer.ts
@@ -3,7 +3,12 @@ import { BucketsNames } from "@/app/API/files-service/functions";
 export enum submitFileAction {
     SET_FILE = 'setFile',
     SET_FILE_TYPE = 'setFileType',
-    SET_EXERCISE_TYPE = 'updateExerciseType',
+    // SET_EXERCISE_TYPE = 'updateExerciseType',
+
+    SET_MAIN_ID = "setMainId",
+    SET_SUB_TYPE_ID = "setSubTypeId",
+    SET_MODEL_ID = "modelId",
+
     UPDATE_METADATA = 'updateMetadata',
     REMOVE_FILE = 'removeFile'
 }
@@ -20,15 +25,26 @@ const deepUpdate = (obj: any, path: string, value: any): any => {
 
 type Action =
     | { type: submitFileAction.SET_FILE, payload: File | null }
+    // | { type: submitFileAction.SET_FILE_TYPE, payload: BucketsNames | null }
+    // | { type: submitFileAction.SET_EXERCISE_TYPE, payload: ExercisesTypes }
+
+    | { type: submitFileAction.SET_MAIN_ID, payload: string | null }
+    | { type: submitFileAction.SET_SUB_TYPE_ID, payload: string | null }
+    | { type: submitFileAction.SET_MODEL_ID, payload: string | null }
+
+    | { type: submitFileAction.REMOVE_FILE, payload: string | null }
     | { type: submitFileAction.SET_FILE_TYPE, payload: BucketsNames | null }
-    | { type: submitFileAction.SET_EXERCISE_TYPE, payload: ExercisesTypes }
+
     | { type: submitFileAction.UPDATE_METADATA, field: string, value: any }
     | { type: submitFileAction.REMOVE_FILE };
 
 export interface submitFileDataType {
     file: File | null;
+    mainId: string | null;
+    subtypeId: string | null;
+    modelId: string | null;
     fileType: BucketsNames | null;
-    exerciseType: ExercisesTypes | null;
+
     metadata: Partial<Metadata>;
 }
 
@@ -40,6 +56,14 @@ export const submitFileReducer = (
         case submitFileAction.SET_FILE:
             return { ...state, file: action.payload };
 
+        case submitFileAction.SET_MAIN_ID:
+            return { ...state, mainId: action.payload };
+        case submitFileAction.SET_SUB_TYPE_ID:
+            return { ...state, subtypeId: action.payload };
+        case submitFileAction.SET_MODEL_ID:
+            return { ...state, modelId: action.payload };
+
+
         case submitFileAction.SET_FILE_TYPE:
             if (action.payload === BucketsNames.IMAGES) {
                 return { ...state, fileType: action.payload, metadata: {} as Partial<ImageMetadata> };
@@ -49,28 +73,33 @@ export const submitFileReducer = (
             }
             return state;
 
-        case submitFileAction.SET_EXERCISE_TYPE:
-            return { ...state, exerciseType: action.payload };
+        // case submitFileAction.SET_EXERCISE_TYPE:
+        //     return { ...state, exerciseType: action.payload };
 
         case submitFileAction.UPDATE_METADATA:
-            // console.log(action.payload, { ...state.metadata, ...action.payload });
             return {
                 ...state,
                 metadata: deepUpdate(state.metadata, action.field, action.value),
 
-                // metadata: {
-                //     ...state.metadata,
-                //     [action.field]: action.value,
-                // },
             };
 
         case submitFileAction.REMOVE_FILE:
             if (state.fileType === BucketsNames.RECORDS) {
-                return { file: null, metadata: { record_length: 0 } as Partial<RecordMetadata>, fileType: state.fileType, exerciseType: state.exerciseType }
+                return {
+                    file: null, metadata: { record_length: 0 } as Partial<RecordMetadata>,
+                    mainId: state.mainId,
+                    subtypeId: state.subtypeId,
+                    modelId: state.modelId,
+                    fileType: state.fileType,
+                }
             }
             else {
                 return {
-                    file: null, metadata: {} as Partial<ImageMetadata>, fileType: state.fileType, exerciseType: state.exerciseType
+                    file: null, metadata: {} as Partial<ImageMetadata>,
+                    mainId: state.mainId,
+                    subtypeId: state.subtypeId,
+                    modelId: state.modelId,
+                    fileType: state.fileType,
                 }
             }
 
