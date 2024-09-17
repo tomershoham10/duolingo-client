02f568300b9c658519633966f2fd5d10572cfd32
tomershoham10 <tomershoham10@gmail.com>
2024-09-17 12:05:13 +0300

    feat: useModelsTableData + fetchModelFiles

--- src/app/classroom/files/page.tsx
+++ src/app/classroom/files/page.tsx
@@ -1,142 +1,136 @@
 'use client';
 import { useStore } from 'zustand';
 import { useInfoBarStore } from '@/app/store/stores/useInfoBarStore';
-import FilesTable from './_FilesTable';
-import { useCallback, useEffect, useMemo, useState } from 'react';
-import Table, { TableHead, TableRow } from '@/components/Table/page';
+import { useCallback, useState } from 'react';
+import Table, { TableHead } from '@/components/Table/page';
 import pRetry from 'p-retry';
-import {
-  BucketsNames,
-  getFileByBucketName,
-} from '@/app/API/files-service/functions';
 import { useFetchTargets } from '@/app/_utils/hooks/(dropdowns)/useFechTargets';
-import { useFetchCountries } from '@/app/_utils/hooks/(dropdowns)/useFechCountries';
-import { useFetchOrganizations } from '@/app/_utils/hooks/(dropdowns)/useFechOrganizations';
+import useModelsTableData from '@/app/_utils/hooks/useModelsTableData';
+import Button, { ButtonColors, ButtonTypes } from '@/components/Button/page';
+import { faArrowUpFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { PopupsTypes, usePopupStore } from '@/app/store/stores/usePopupStore';
+import UploadFilePopup from '@/app/(popups)/(upload)/UploadFilePopup/page';
+import { getModelsFiles } from '@/app/API/files-service/functions';
 
 const Files: React.FC = () => {
-  console.log('records');
-  const targetsList = useFetchTargets().filter((target) => target.level === 3);
+  const targetsList = useFetchTargets();
+  const modelsTableData = useModelsTableData();
 
-  const countiresList = useFetchCountries();
-  const organizationsList = useFetchOrganizations();
+  const [selectedModelsRowIndex, setSelectedModelsRowIndex] =
+    useState<number>(-1);
+  const [selectedFilesRowIndex, setSelectedFilesRowIndex] =
+    useState<number>(-1);
 
-  const memoizedValue = useMemo(() => {
-    const data: any[] = [];
-    let organizationName: string | null = null;
-    let countryName: string | null = null;
-    targetsList.forEach((target) => {
-      const targetOrg = target.organization;
-      if (targetOrg) {
-        const orgId = targetOrg[0];
-        const organizationObj = organizationsList.find(
-          (org) => org._id === orgId
-        );
-        organizationName = organizationObj?.organization_name || null;
-        if (organizationObj) {
-          const countryObj = countiresList.find(
-            (country) => country._id === organizationObj.country
-          );
-          countryName = countryObj?.country_name || null;
-        }
-      }
-      data.push({
-        country: countryName || 'N/A',
-        organization: organizationName || 'N/A',
-        name: target.name,
-      });
-    });
-
-    return data;
-  }, [countiresList, organizationsList, targetsList]);
-  console.log('memoizedValue',memoizedValue);
+  console.log('modelsTableData', modelsTableData);
 
   const updateSelectedFile = useInfoBarStore.getState().updateSelectedFile;
+  const updateSelectedPopup = usePopupStore.getState().updateSelectedPopup;
 
-  const TABLE_HEAD: TableHead[] = [
+  const modelsTableHead: TableHead[] = [
     { key: 'country', label: 'Country' },
     { key: 'organization', label: 'Organization' },
-    { key: 'mainType', label: 'main type' },
-    { key: 'subType', label: 'sub type' },
-    { key: 'model', label: 'name' },
+    { key: 'mainTypeName', label: 'main type' },
+    { key: 'subTypeName', label: 'sub type' },
+    { key: 'modelName', label: 'name' },
   ];
 
-  const [recordsData, setRecordsData] = useState<FileType[]>([]);
+  const filesTableHead: TableHead[] = [
+    { key: 'name', label: 'File name' },
+    { key: 'type', label: 'File type' },
+    { key: 'hasMetadata', label: 'Has metadata' },
+  ];
 
-  const [tableData, setTableData] = useState<TableRow[]>([]);
+  const [filesData, setFilesData] = useState<FileType[]>([]);
 
-  //   const fetchData = useCallback(async () => {
-  //     try {
-  //       const level1IdsList = targetsList
-  //         .filter((target) => target.level === 1)
-  //         .map((target) => target._id);
-
-  //       const fetchPromises = level1IdsList.map((level1Id) =>
-  //         pRetry(() => getFileByBucketName(level1Id), { retries: 5 })
-  //       );
-
-  //       // Wait for all promises to resolve
-  //       const results = await Promise.all(fetchPromises);
-  //       console.log('files fetch data', results);
-  //       // Flatten the results if needed and update state
-  //       const allRecordsData = results.flat() as FileType[];
-  //       setRecordsData(allRecordsData || []);
-  //     } catch (error) {
-  //       console.error('Failed to fetch records:', error);
-  //     }
-  //   }, [targetsList]);
-
-  //   useEffect(() => {
-  //     console.log('records fetchData');
-  //     fetchData();
-  //   }, [fetchData]);
-
-  //   useEffect(() => {
-  //     console.log('recordsData triggered', recordsData);
-  //     setTableData(
-  //       recordsData.map((data) => {
-  //         return {
-  //           ...data,
-  //           fileType: data.name.endsWith('wav')
-  //             ? BucketsNames.RECORDS
-  //             : BucketsNames.IMAGES,
-  //         };
-  //       })
-  //     );
-  //   }, [recordsData]);
-
-  const handleSelectTableRow = useCallback(
-    (item: any) => {
-      console.log('item', item);
-
-      const modifiedRecord = {
-        id: item.id,
-        name: item.name,
-        exerciseType: item.exerciseType,
-        metadata: { ...item.metadata },
-      };
-
-      console.log('modifiedRecord', modifiedRecord);
-      updateSelectedFile(modifiedRecord);
+  const fetchModelFiles = useCallback(
+    async (mainId: string, subTypeId: string, modelId: string) => {
+      const res = await pRetry(
+        () => getModelsFiles(mainId, subTypeId, modelId),
+        {
+          retries: 5,
+        }
+      );
+      console.log('fetchModelFiles1', res);
+      if (res) {
+        if (Object.values(res).length > 0) {
+          // else - model has no files
+          const imagesData = res[subTypeId][modelId]['images'];
+          const recordsData = res[subTypeId][modelId]['records'];
+          const comboData = [
+            ...recordsData.map((rec) => {
+              return { ...rec, type: 'record' };
+            }),
+            ...imagesData.map((img) => {
+              return { ...img, type: 'image' };
+            }),
+          ];
+          setFilesData(
+            comboData.map((file) => {
+              return {
+                ...file,
+                hasMetadata:
+                  Object.values(file.metadata).length > 0 ? 'yes' : 'no',
+              };
+            })
+          );
+          console.log('fetchModelFiles2', imagesData, recordsData, [
+            ...recordsData,
+            ...imagesData,
+          ]);
+        } else {
+          setFilesData([]);
+        }
+      } else {
+        alert('error');
+        setFilesData([]);
+      }
     },
-    [updateSelectedFile]
+    []
   );
 
-  const selectedRowIndex = useMemo(() => {
-    return undefined;
+  const handleSelectModelsRow = useCallback(
+    (row: any, index: number) => {
+      console.log('handleSelectModelsRow', row);
+      fetchModelFiles(row.mainTypeId, row.subTypeId, row.modelId);
+      setSelectedModelsRowIndex(index);
+    },
+    [fetchModelFiles]
+  );
+
+  const handleSelectFilesRow = useCallback((row: any, index: number) => {
+    console.log('handleSelectFilesRow', row);
+    setSelectedFilesRowIndex(index);
   }, []);
+
   return (
-    <section className='h-full p-6 text-duoGray-darkest dark:text-duoGrayDark-lightest'>
-      {/* <FilesTable
-        tableData={tableData}
-        onSelect={handleSelectTableRow}
-        selectedRowIndex={selectedRowIndex}
-      /> */}
+    <section className='mx-auto flex h-full w-fit flex-col gap-6 p-6 text-duoGray-darkest dark:text-duoGrayDark-lightest'>
+      <UploadFilePopup />
       <Table
-        headers={TABLE_HEAD}
-        rows={tableData}
-        onSelect={() => {}}
-        selectedRowIndex={selectedRowIndex}
+        headers={modelsTableHead}
+        rows={modelsTableData}
+        onSelect={handleSelectModelsRow}
+        selectedRowIndex={selectedModelsRowIndex}
+        maxHight={'max-h-[306px]'}
       />
+
+      <Table
+        headers={filesTableHead}
+        rows={filesData}
+        onSelect={handleSelectFilesRow}
+        selectedRowIndex={selectedFilesRowIndex}
+        maxHight={'max-h-[306px]'}
+      />
+
+      <div className='relative flex items-center justify-start py-8'>
+        <Button
+          label={'UPLOAD'}
+          buttonType={ButtonTypes.SUBMIT}
+          color={ButtonColors.BLUE}
+          icon={faArrowUpFromBracket}
+          //   loadingLabel={'Uploading...'}
+          onClick={() => updateSelectedPopup(PopupsTypes.UPLOAD_RECORD)}
+        />
+      </div>
     </section>
   );
 };
