56fa78c5091eaff01081dc976933ff12be7cf0b6
tomershoham10 <tomershoham10@gmail.com>
2024-09-17 13:47:48 +0300

    refactor: Table props to const {}=props

--- src/components/Table/page.tsx
+++ src/components/Table/page.tsx
@@ -10,25 +10,29 @@ export interface TableRow {
 interface DefaultTableProps {
   headers: TableHead[];
   rows: TableRow[];
-  onSelect?: (row: any) => void;
+  onSelect?: (row: any, index: number) => void;
   selectedRowIndex?: number;
+  maxHight?: string;
 }
 
 const Table = (props: DefaultTableProps) => {
-  const headers = props.headers;
-  const tableData = props.rows;
-  const onSelect = props.onSelect;
+  const { headers, rows, onSelect, selectedRowIndex, maxHight } = props;
+
+  const maximumHight = maxHight || 'max-h-[70%]';
+
   return (
-    <div className=' flex max-h-[70%] justify-center overflow-auto rounded-lg border-2 dark:border-duoGrayDark-lighter'>
-      <table className=' divide-y-2 divide-duoGray-default dark:bg-duoGrayDark-darkest text-left dark:divide-duoBlueDark-text dark:divide-opacity-70 2xl:text-xl'>
-        <thead className=' uppercase text-duoGray-darkText  dark:text-duoBlueDark-text'>
+    <div
+      className={`flex ${maximumHight} justify-center overflow-auto rounded-lg border-2 dark:border-duoGrayDark-lighter`}
+    >
+      <table className='divide-y-2 divide-duoGray-default text-left dark:divide-duoBlueDark-text dark:divide-opacity-70 dark:bg-duoGrayDark-darkest 2xl:text-xl'>
+        <thead className='uppercase text-duoGray-darkText dark:text-duoBlueDark-text'>
           <tr>
             {headers.map((header) => (
               <th
                 key={header.key}
                 className='h-12 cursor-default px-4 2xl:h-20 2xl:px-8'
               >
-                <span className='flex items-center justify-start font-extrabold '>
+                <span className='flex items-center justify-start font-extrabold'>
                   {header.label}
                 </span>
               </th>
@@ -36,36 +40,33 @@ const Table = (props: DefaultTableProps) => {
           </tr>
         </thead>
         <tbody className='divide-y-2 divide-duoGray-lighter text-duoGray-darker dark:divide-duoGrayDark-dark dark:text-duoGrayDark-lightest'>
-          {tableData.map((row, index) => {
-            const isLast = index === tableData.length - 1;
+          {rows.map((row, index) => {
+            const isLast = index === rows.length - 1;
             const headersKeys = Object.values(headers.map((head) => head.key));
             return (
               <tr
                 key={index}
-                className={`cursor-pointer font-bold hover:bg-duoGray-lighter hover:light:text-duoGray-darkest dark:hover:bg-duoBlueDark-darkest
-                ${
-                  props.selectedRowIndex === index
-                    ? 'bg-duoGray-lighter light:text-duoGray-darkest dark:bg-duoBlueDark-darkest'
+                className={`hover:light:text-duoGray-darkest cursor-pointer font-bold hover:bg-duoGray-lighter dark:hover:bg-duoBlueDark-darkest ${
+                  selectedRowIndex === index
+                    ? 'light:text-duoGray-darkest bg-duoGray-lighter dark:bg-duoBlueDark-darkest'
                     : ''
-                }
-                `}
+                } `}
                 onClick={() => {
-                  onSelect ? onSelect(tableData[index]) : null;
+                  onSelect ? onSelect(rows[index], index) : null;
                 }}
               >
                 {headers.map(({ key }) => (
                   <td
                     key={key}
-                    className={`h-16 px-4 text-center
-                  ${
-                    isLast
-                      ? headersKeys.indexOf(key) === 0
-                        ? 'rounded-bl-lg'
-                        : headersKeys.indexOf(key) === headersKeys.length - 1
-                          ? 'rounded-br-lg'
-                          : ''
-                      : ''
-                  }`}
+                    className={`h-16 px-4 text-center ${
+                      isLast
+                        ? headersKeys.indexOf(key) === 0
+                          ? 'rounded-bl-lg'
+                          : headersKeys.indexOf(key) === headersKeys.length - 1
+                            ? 'rounded-br-lg'
+                            : ''
+                        : ''
+                    }`}
                   >
                     {row[key]}
                   </td>
