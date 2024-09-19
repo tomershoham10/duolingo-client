276fc2a85ae6f0fb6f8a34c9d09f088579e06421
tomershoham10 <tomershoham10@gmail.com>
2024-09-19 15:50:51 +0300

    style: whitespace the text in the files table

--- src/components/InfoBar/createExerciseInfo/_CreateSpotreccInfo/index.tsx
+++ src/components/InfoBar/createExerciseInfo/_CreateSpotreccInfo/index.tsx
@@ -5,6 +5,7 @@ import MetadataSection from '../../(utils)/MetadataSection';
 import { useStore } from 'zustand';
 import { useInfoBarStore } from '@/app/store/stores/useInfoBarStore';
 import { useFetchModelFiles } from '@/app/_utils/hooks/useFetchModelFiles';
+import PlusButton from '@/components/PlusButton/page';
 
 const CreateSpotreccInfo: React.FC = () => {
   const addSubExercise = useCreateSpotreccStore.getState().addSubExercise;
@@ -52,27 +53,28 @@ const CreateSpotreccInfo: React.FC = () => {
   }, [selectedFile]);
 
   return (
-    <div className='flex h-full w-[90%] flex-col gap-3 px-6 py-4'>
+    <div className='flex h-full w-full flex-col gap-3 p-4'>
       <h1>Files</h1>
       <ul className='max-h-[33%] w-full cursor-pointer overflow-hidden rounded-md border-2 border-duoGrayDark-light px-2 py-2 hover:overflow-y-auto'>
         {filesData.map((file, index) => (
           <li
             key={file.name}
-            className={`w-full border-duoGrayDark-dark text-lg ${
+            className={`grid w-full border-duoGrayDark-dark text-lg ${
               index === Object.values(CreateSpotreccInfo).length - 1 ||
               Object.values(CreateSpotreccInfo).length === 1
                 ? 'border-0'
                 : 'border-b-2'
             }`}
           >
-            <p
+            <div
               className='w-full overflow-hidden text-ellipsis whitespace-nowrap p-1 hover:rounded-md hover:bg-duoGrayDark-light'
               onClick={() => {
                 handleFileSelected(file.name);
               }}
+              title={file.name} // Tooltip for long names
             >
               {file.name}
-            </p>
+            </div>
           </li>
         ))}
       </ul>
@@ -82,7 +84,7 @@ const CreateSpotreccInfo: React.FC = () => {
           fileName={selectedFile.name}
           metadata={selectedFile.metadata}
         >
-          <button
+          <PlusButton
             onClick={() => {
               console.log('add sub exercise', {
                 fileName: selectedFile.name,
@@ -97,9 +99,7 @@ const CreateSpotreccInfo: React.FC = () => {
                 bufferTime: 15,
               });
             }}
-          >
-            addSubExercise
-          </button>
+          />
         </MetadataSection>
       )}
     </div>
