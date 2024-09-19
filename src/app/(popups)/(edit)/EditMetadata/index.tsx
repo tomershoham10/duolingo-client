43ebcd02d39fca8edfb118cb8431355c1e39f8f4
tomershoham10 <tomershoham10@gmail.com>
2024-09-19 10:52:12 +0300

    feat: records/images edit metadata components by file's type

--- src/app/(popups)/(edit)/EditMetadata/index.tsx
+++ src/app/(popups)/(edit)/EditMetadata/index.tsx
@@ -1,7 +1,13 @@
+import { lazy } from 'react';
 import { useStore } from 'zustand';
 import { PopupsTypes } from '@/app/store/stores/usePopupStore';
 import PopupHeader from '../../PopupHeader/page';
 import { useInfoBarStore } from '@/app/store/stores/useInfoBarStore';
+
+const RecordsMetadataPopup = lazy(
+  () => import('./(subPages)/_RecordsMetadata')
+);
+const ImagesMetadataPopup = lazy(() => import('./(subPages)/_ImagesMetadata'));
 
 const EditMetadataPopup: React.FC = () => {
   const mainTypeId = useStore(
@@ -14,24 +20,35 @@ const EditMetadataPopup: React.FC = () => {
   );
   const model = useStore(useInfoBarStore, (state) => state.selectedModel);
   const selectedFile = useStore(useInfoBarStore, (state) => state.selectedFile);
+
   return (
     <PopupHeader
       popupType={PopupsTypes.EDIT_METADATA}
-      header={`Edit metadata - ${selectedFile?.name}`}
+      header={`Edit metadata${selectedFile?.name && ` - ${selectedFile}`}`}
       onClose={() => {}}
     >
-      {selectedFile?.name?.endsWith('.wav') ? (
-        <div className='mt-12 grid w-full grid-cols-4 grid-rows-6 gap-y-4 px-4 py-4 3xl:gap-y-12'>
-          <p className='col-span-1 flex-none text-lg font-bold text-duoGray-darkest dark:text-duoGrayDark-lightest'>
-            records
-          </p>
-        </div>
+      {mainTypeId && subTypeId && model && selectedFile ? (
+        selectedFile.name?.endsWith('.wav') ? (
+          <div className='mt-12 grid w-full grid-cols-4 grid-rows-6 gap-y-4 px-4 py-4 3xl:gap-y-12'>
+            <RecordsMetadataPopup
+              mainTypeId={mainTypeId}
+              subTypeId={subTypeId}
+              model={model}
+              selectedFile={selectedFile}
+            />
+          </div>
+        ) : (
+          <div className='mt-12 grid w-full grid-cols-4 grid-rows-6 gap-y-4 px-4 py-4 3xl:gap-y-12'>
+            <ImagesMetadataPopup
+              mainTypeId={mainTypeId}
+              subTypeId={subTypeId}
+              model={model}
+              selectedFile={selectedFile}
+            />
+          </div>
+        )
       ) : (
-        <div className='mt-12 grid w-full grid-cols-4 grid-rows-6 gap-y-4 px-4 py-4 3xl:gap-y-12'>
-          <p className='col-span-1 flex-none text-lg font-bold text-duoGray-darkest dark:text-duoGrayDark-lightest'>
-            Images
-          </p>
-        </div>
+        <>loading...</>
       )}
     </PopupHeader>
   );
