3c0f5a19c934f6be303c6643ca7e2bf82ba87045
tomershoham10 <tomershoham10@gmail.com>
2024-09-24 16:36:00 +0300

    refactor: removed unused old code

--- src/components/InfoBar/createExerciseInfo/page.tsx
+++ src/components/InfoBar/createExerciseInfo/page.tsx
@@ -1,13 +1,3 @@
-// 'use client';
-// import { useCallback, useEffect, useState } from 'react';
-// import pRetry from 'p-retry';
-// import { useCreateSpotreccStore } from '@/app/store/stores/(createExercises)/useCreateSpotreccStore';
-// import {
-//   BucketsNames,
-//   getFileByBucketAndType,
-// } from '@/app/API/files-service/functions';
-// import { useCreateFsaStore } from '@/app/store/stores/(createExercises)/useCreateFsaStore';
-// import MetadataSection from '../(utils)/MetadataSection';
 import { ExercisesTypes } from '@/app/API/classes-service/exercises/functions';
 import CreateSpotreccInfo from './_CreateSpotreccInfo';
 import CreateFsaInfo from './_CreateFsaInfo';
@@ -23,57 +13,6 @@ const CreateExerciseInfo: React.FC<CreateExerciseInfoProps> = (props) => {
 
     [ExercisesTypes.SPOTRECC]: <CreateSpotreccInfo />,
   };
-  //   const setFileName = useCreateFsaStore.getState().setFileName;
-
-  //   const updateRecordLength = useCreateFsaStore.getState().updateRecordLength;
-  //   const addSubExercise = useCreateSpotreccStore.getState().addSubExercise;
-  //   const [recordsData, setRecordsData] = useState<FileType[]>([]);
-  //   const [selectedFile, setSelectedFile] = useState<FileType | null>(null);
-
-  //   // need to fetch data by model
-  //   const fetchData = useCallback(async () => {
-  //     try {
-  //       const res = (await pRetry(
-  //         () => getFileByBucketAndType(BucketsNames.RECORDS, exerciseType),
-  //         { retries: 5 }
-  //       )) as FileType[];
-  //       //   console.log('SpotreccFiles', res);
-  //       setRecordsData(res);
-  //     } catch (error) {
-  //       console.error('Failed to fetch records:', error);
-  //     }
-  //   }, [exerciseType]);
-
-  //   useEffect(() => {
-  //     fetchData();
-  //   }, [fetchData]);
-
-  //   const handleFileSelected = useCallback(
-  //     (selectedFileName: string, recordLength?: number) => {
-  //       console.log(
-  //         'handleFileSelected',
-  //         recordsData,
-  //         exerciseType === ExercisesTypes.FSA
-  //       );
-  //       setSelectedFile(
-  //         recordsData.filter((item) => item.name === selectedFileName)[0]
-  //       );
-  //       if (exerciseType === ExercisesTypes.FSA) {
-  //         console.log('fsa - setFileName', selectedFileName);
-  //         setFileName(selectedFileName);
-  //         updateRecordLength(recordLength || 0);
-  //       }
-  //     },
-  //     [exerciseType, recordsData, setFileName, updateRecordLength]
-  //   );
-
-  //   useEffect(() => {
-  //     console.log('CreateExerciseInfo - recordsData', recordsData);
-  //   }, [recordsData]);
-
-  //   useEffect(() => {
-  //     console.log('CreateExerciseInfo - selectedFile', selectedFile);
-  //   }, [selectedFile]);
 
   return (
     <section className='h-full w-full overflow-hidden'>
@@ -81,64 +20,6 @@ const CreateExerciseInfo: React.FC<CreateExerciseInfoProps> = (props) => {
         {components[exerciseType]}
       </div>
     </section>
-    // <div className='flex h-full w-[90%] flex-col gap-3 px-6 py-4'>
-    //   <h1>{exerciseType} Files</h1>
-    //   <ul className='max-h-[33%] w-full cursor-pointer overflow-hidden rounded-md border-2 border-duoGrayDark-light px-2 py-2 hover:overflow-y-auto'>
-    //     {recordsData.map((file, index) => (
-    //       <li
-    //         key={file.name}
-    //         className={`w-full border-duoGrayDark-dark text-lg ${
-    //           index === Object.values(recordsData).length - 1 ||
-    //           Object.values(recordsData).length === 1
-    //             ? 'border-0'
-    //             : 'border-b-2'
-    //         }`}
-    //       >
-    //         <p
-    //           className='w-full overflow-hidden text-ellipsis whitespace-nowrap p-1 hover:rounded-md hover:bg-duoGrayDark-light'
-    //           onClick={() => {
-    //             if (exerciseType === ExercisesTypes.FSA) {
-    //               const fileMetadata = file.metadata as FSAMetadata;
-    //               handleFileSelected(file.name, fileMetadata.record_length);
-    //             } else {
-    //               handleFileSelected(file.name);
-    //             }
-    //           }}
-    //         >
-    //           {file.name}
-    //         </p>
-    //       </li>
-    //     ))}
-    //   </ul>
-
-    //   {selectedFile && (
-    //     <MetadataSection
-    //       fileName={selectedFile.name}
-    //       metadata={selectedFile.metadata}
-    //     >
-    //       {exerciseType === ExercisesTypes.SPOTRECC && (
-    //         <button
-    //           onClick={() => {
-    //             console.log('add sub exercise', {
-    //               fileName: selectedFile.name,
-    //               description: null,
-    //               exerciseTime: 15,
-    //               bufferTime: 15,
-    //             });
-    //             addSubExercise({
-    //               fileName: selectedFile.name,
-    //               description: null,
-    //               exerciseTime: 15, //secs
-    //               bufferTime: 15,
-    //             });
-    //           }}
-    //         >
-    //           addSubExercise
-    //         </button>
-    //       )}
-    //     </MetadataSection>
-    //   )}
-    // </div>
   );
 };
 
