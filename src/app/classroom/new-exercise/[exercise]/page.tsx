6aac6c83e99f2c2a5811750c94e42da57356a0a6
tomershoham10 <tomershoham10@gmail.com>
2024-09-24 16:33:19 +0300

    style: header height + padding

--- src/app/classroom/new-exercise/[exercise]/page.tsx
+++ src/app/classroom/new-exercise/[exercise]/page.tsx
@@ -1,7 +1,6 @@
 import { ExercisesTypes } from '@/app/API/classes-service/exercises/functions';
 import CreateFsa from './_CreateFsa/page';
 import CreateSpotrecc from './_CreateSpotrecc/page';
-import TargetsDropdowns from '@/components/TargetsDropdowns/index';
 
 const NewExercise = ({ params }: { params: { exercise: ExercisesTypes } }) => {
   console.log('params.exercise', params.exercise);
@@ -13,8 +12,8 @@ const NewExercise = ({ params }: { params: { exercise: ExercisesTypes } }) => {
   };
 
   return (
-    <section className='flex h-full w-full flex-col overflow-x-hidden px-10 2xl:px-16 3xl:pt-4'>
-      <section className='flex h-[5.5rem] items-center gap-3 text-4xl font-extrabold uppercase'>
+    <section className='flex h-full w-full flex-col overflow-x-hidden px-10 justify-start'>
+      <section className='flex items-center gap-3 text-4xl font-extrabold mt-6 uppercase'>
         <p> create</p>
         <p className='text-duoGrayDark-lighter'> {params.exercise}</p>
       </section>
