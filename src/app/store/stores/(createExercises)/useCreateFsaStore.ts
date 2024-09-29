00e5152a10fcba0a7c57faf8e60d8e001f3e2248
tomershoham10 <tomershoham10@gmail.com>
2024-09-29 16:29:04 +0300

    commented out the file

--- src/app/store/stores/(createExercises)/useCreateFsaStore.ts
+++ src/app/store/stores/(createExercises)/useCreateFsaStore.ts
@@ -1,37 +1,37 @@
-"use client"
-import { mountStoreDevtool } from 'simple-zustand-devtools';
-import { create } from 'zustand';
+// "use client"
+// import { mountStoreDevtool } from 'simple-zustand-devtools';
+// import { create } from 'zustand';
 
 
-type CreateFsaState = {
-    fileName: string | null;
-    recordLength: number;
-}
+// type CreateFsaState = {
+//     fileName: string | null;
+//     recordLength: number;
+// }
 
-type Action = {
-    setFileName: (fileName: string | null) => void;
-    updateRecordLength: (recordLength: number) => void;
-    resetStore: () => void;
-}
+// type Action = {
+//     setFileName: (fileName: string | null) => void;
+//     updateRecordLength: (recordLength: number) => void;
+//     resetStore: () => void;
+// }
 
-export const useCreateFsaStore = create<CreateFsaState & Action>(
-    (set) => ({
-        fileName: null,
-        recordLength: 0,
-        description: undefined,
+// export const useCreateFsaStore = create<CreateFsaState & Action>(
+//     (set) => ({
+//         fileName: null,
+//         recordLength: 0,
+//         description: undefined,
 
-        setFileName: (fileName) => set(() => ({ fileName: fileName })),
-        updateRecordLength: (recordLength) => set(() => ({ recordLength: recordLength })),
-        resetStore: () => {
-            set(() => ({
-                fileName: null,
-                recordLength: 0
-            }));
-        }
-    })
-)
+//         setFileName: (fileName) => set(() => ({ fileName: fileName })),
+//         updateRecordLength: (recordLength) => set(() => ({ recordLength: recordLength })),
+//         resetStore: () => {
+//             set(() => ({
+//                 fileName: null,
+//                 recordLength: 0
+//             }));
+//         }
+//     })
+// )
 
-if (process.env.NODE_ENV === 'development') {
-    mountStoreDevtool('useCreateFsaStore', useCreateFsaStore);
-}
+// if (process.env.NODE_ENV === 'development') {
+//     mountStoreDevtool('useCreateFsaStore', useCreateFsaStore);
+// }
 
