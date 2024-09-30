c54b5d6e7523c43a95cb1fc3b79d73539c0a0d87
tomershoham10 <tomershoham10@gmail.com>
2024-09-30 16:07:58 +0300

    syle: plus icon color

--- src/components/PlusButton/page.tsx
+++ src/components/PlusButton/page.tsx
@@ -4,12 +4,12 @@ const PlusButton: React.FC<PlusButtonProps> = (props) => {
   const { label, onClick } = props;
   return (
     <div
-      className={`my-3 flex cursor-pointer flex-row items-center justify-start ${
-        !!label ? 'group' : ''
+      className={`my-3 flex cursor-pointer flex-row items-center justify-start w-fit mx-auto ${
+        !!label && 'group'
       }`}
     >
       <button
-        className={`flex h-8 w-8 items-center justify-center rounded-full bg-duoGray-lighter text-2xl dark:bg-duoGrayDark-light lg:h-9 lg:w-9  ${
+        className={`flex h-8 w-8 items-center justify-center rounded-full bg-duoGray-lighter text-2xl text-duoGrayDark-lightest dark:bg-duoGrayDark-light lg:h-9 lg:w-9 ${
           !!label
             ? 'group-hover:w-fit group-hover:rounded-2xl group-hover:bg-duoGray-hover group-hover:px-2 group-hover:py-3 dark:group-hover:bg-duoGrayDark-lighter'
             : 'hover:bg-duoGray-hover dark:hover:bg-duoGrayDark-lighter'
