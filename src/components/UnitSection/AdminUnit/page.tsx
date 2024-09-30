ca89695752339aab674ad2eee4dc6a24ff94fca6
tomershoham10 <tomershoham10@gmail.com>
2024-09-30 14:44:05 +0300

    feat: AdminUnitLevelsSection component

--- src/components/UnitSection/AdminUnit/page.tsx
+++ src/components/UnitSection/AdminUnit/page.tsx
@@ -1,29 +1,14 @@
 import { useCallback, useEffect, useReducer, useState } from 'react';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faBook,
-  faChevronDown,
-  faPenToSquare,
-  faStar,
-} from '@fortawesome/free-solid-svg-icons';
 import {
   fieldToEditType,
   useInfoBarStore,
 } from '@/app/store/stores/useInfoBarStore';
-import {
-  DataWithFatherId,
-  courseDataReducer,
-} from '@/reducers/courseDataReducer';
-import { useAlertStore } from '@/app/store/stores/useAlertStore';
+import { courseDataReducer } from '@/reducers/courseDataReducer';
 import useCourseData from '@/app/_utils/hooks/useCourseData';
-import LodingAdminSection from './LodingAdminSection/page';
-import AdminUnitHeader from './AdminUnitHeader/page';
-import AdminUnitAccourdion from './AdminUnitAccourdion/page';
+import LodingAdminSection from './(components)/LodingAdminSection/page';
+import AdminUnitHeader from './(components)/AdminUnitHeader/page';
 import { useFetchTargets } from '@/app/_utils/hooks/(dropdowns)/useFechTargets';
-
-library.add(faBook, faChevronDown, faPenToSquare, faStar);
+import AdminUnitLevelsSection from './(components)/AdminUnitLevelsSection';
 
 interface AdminUnitProps {
   courseId: string;
@@ -32,7 +17,6 @@ interface AdminUnitProps {
 const AdminUnit: React.FC<AdminUnitProps> = (props) => {
   const propsCourseId = props.courseId;
 
-  const addAlert = useAlertStore.getState().addAlert;
   const targetsList = useFetchTargets();
 
   const fieldId = useInfoBarStore.getState().syllabusFieldId;
@@ -88,15 +72,6 @@ const AdminUnit: React.FC<AdminUnitProps> = (props) => {
       : setExerciseAccordion((pervExercises) => [...pervExercises, exerciseId]);
   };
 
-  const isIdInDataWithFatherIdObj = (
-    id: string,
-    obj: DataWithFatherId<LevelType | LessonType | ExerciseType>[]
-  ): boolean => {
-    return obj.some((levelData) => {
-      return levelData.data.some((doc) => doc._id === id);
-    });
-  };
-
   const updateInfobarData = useCallback(
     (
       filedType: fieldToEditType,
@@ -131,22 +106,19 @@ const AdminUnit: React.FC<AdminUnitProps> = (props) => {
             <div
               key={unit._id}
               className={`flex-none py-[2rem] ${
-                courseDataState.suspendedUnitsIds.includes(unit._id)
-                  ? 'opacity-60'
-                  : ''
+                courseDataState.suspendedUnitsIds.includes(unit._id) &&
+                'opacity-60'
               } `}
             >
-              <div className='flex-col'>
-                <AdminUnitHeader
-                  unit={unit}
-                  unitIndex={unitIndex}
-                  courseId={propsCourseId}
-                  isSuspended={courseDataState.suspendedUnitsIds.includes(
-                    unit._id
-                  )}
-                  updateInfobarData={updateInfobarData}
-                />
-
+              <AdminUnitHeader
+                unit={unit}
+                unitIndex={unitIndex}
+                courseId={propsCourseId}
+                isSuspended={courseDataState.suspendedUnitsIds.includes(
+                  unit._id
+                )}
+                updateInfobarData={updateInfobarData}
+              >
                 <div className='flex flex-col'>
                   {courseDataState.levels &&
                     courseDataState.levels.length > 0 &&
@@ -154,171 +126,22 @@ const AdminUnit: React.FC<AdminUnitProps> = (props) => {
                       (levelsObject, levelsObjectIndex) => (
                         <div key={levelsObjectIndex} className='flex-none'>
                           {levelsObject.fatherId === unit._id && (
-                            <div className='flex flex-col'>
-                              {levelsObject.data.length > 0 &&
-                                levelsObject.data.map((level, levelIndex) => (
-                                  <div
-                                    key={level._id}
-                                    className={
-                                      levelIndex ===
-                                      courseDataState.levels.length
-                                        ? 'flex h-fit flex-col rounded-b-lg border-2 border-t-0 border-duoGray-light px-6 py-3 dark:border-duoGrayDark-dark'
-                                        : 'flex h-fit flex-col border-2 border-t-0 border-duoGray-light px-6 py-3 dark:border-duoGrayDark-dark'
-                                    }
-                                  >
-                                    <div className='w-full'>
-                                      {courseDataState.lessons &&
-                                        courseDataState.lessons.length > 0 &&
-                                        courseDataState.lessons.map(
-                                          (
-                                            lessonsObject,
-                                            lessonsObjectIndex
-                                          ) => (
-                                            <div key={lessonsObjectIndex}>
-                                              {lessonsObject.fatherId ===
-                                                level._id && (
-                                                <div className='divide-y-2 divide-duoGray-hover'>
-                                                  {lessonsObject.data.length >
-                                                    0 &&
-                                                    lessonsObject.data.map(
-                                                      (lesson, lessonIndex) => (
-                                                        <div
-                                                          key={lesson._id}
-                                                          className='flex w-full flex-row pt-4 text-base font-medium'
-                                                        >
-                                                          <div
-                                                            className='h-12 w-12 flex-none cursor-pointer rounded-full bg-duoGreen-default font-extrabold text-white'
-                                                            onClick={() => {
-                                                              const isSuspended =
-                                                                unit.suspendedLevelsIds.includes(
-                                                                  level._id
-                                                                );
-                                                              updateInfobarData(
-                                                                fieldToEditType.LEVEL,
-                                                                level._id,
-                                                                levelIndex,
-                                                                level.lessonsIds,
-                                                                unit._id,
-                                                                isSuspended
-                                                              );
-                                                            }}
-                                                          >
-                                                            <div className='mx-auto my-auto flex h-full flex-col items-center justify-center'>
-                                                              <div className='mt-1 flex h-full flex-col items-center justify-center'>
-                                                                <FontAwesomeIcon
-                                                                  icon={faStar}
-                                                                  size='lg'
-                                                                />
-                                                                <span className='h-fit text-[11px]'>
-                                                                  {levelIndex +
-                                                                    1}
-                                                                  -
-                                                                  {lessonIndex +
-                                                                    1}
-                                                                </span>
-                                                              </div>
-                                                            </div>
-                                                          </div>
-                                                          <div className='mx-6 w-full'>
-                                                            <button
-                                                              className='text-conter flex w-full items-center justify-start font-extrabold text-duoGray-dark dark:text-duoGrayDark-lightest'
-                                                              onClick={() => {
-                                                                const isSuspended =
-                                                                  level.suspendedLessonsIds.includes(
-                                                                    lesson._id
-                                                                  );
-                                                                updateInfobarData(
-                                                                  fieldToEditType.LESSON,
-                                                                  lesson._id,
-                                                                  lessonIndex,
-                                                                  lesson.exercisesIds,
-                                                                  level._id,
-                                                                  isSuspended
-                                                                );
-                                                              }}
-                                                            >
-                                                              <span>
-                                                                LESSON
-                                                                {' - '}
-                                                                {lesson.name}
-                                                              </span>
-                                                            </button>
-                                                            <div className='flex w-full flex-col'>
-                                                              {courseDataState.exercises &&
-                                                                courseDataState
-                                                                  .exercises
-                                                                  .length > 0 &&
-                                                                courseDataState.exercises.map(
-                                                                  (
-                                                                    exerciseObject,
-                                                                    exerciseObjectIndex
-                                                                  ) => (
-                                                                    <div
-                                                                      key={
-                                                                        exerciseObjectIndex
-                                                                      }
-                                                                      className='w-full divide-y-2 dark:divide-duoGrayDark-light'
-                                                                    >
-                                                                      {exerciseObject.fatherId ===
-                                                                        lesson._id &&
-                                                                        exerciseObject
-                                                                          .data
-                                                                          .length >
-                                                                          0 &&
-                                                                        exerciseObject.data.map(
-                                                                          (
-                                                                            exercise,
-                                                                            exerciseIndex
-                                                                          ) => (
-                                                                            <section
-                                                                              key={
-                                                                                exerciseIndex
-                                                                              }
-                                                                            >
-                                                                              <AdminUnitAccourdion
-                                                                                exercise={
-                                                                                  exercise
-                                                                                }
-                                                                                exerciseIndex={
-                                                                                  exerciseIndex
-                                                                                }
-                                                                                isOpen={exerciseAccordion.includes(
-                                                                                  exercise._id
-                                                                                )}
-                                                                                targetsList={
-                                                                                  targetsList ||
-                                                                                  []
-                                                                                }
-                                                                                toggleAccordion={
-                                                                                  toggleAccordion
-                                                                                }
-                                                                              />
-                                                                            </section>
-                                                                          )
-                                                                        )}
-                                                                    </div>
-                                                                  )
-                                                                )}
-                                                            </div>
-                                                          </div>
-                                                        </div>
-                                                      )
-                                                    )}
-                                                </div>
-                                              )}
-                                            </div>
-                                          )
-                                        )}
-                                    </div>
-                                  </div>
-                                ))}
-                            </div>
+                            <AdminUnitLevelsSection
+                              unitId={unit._id}
+                              levelsData={levelsObject.data}
+                              courseDataState={courseDataState}
+                              suspendedLevelsIds={unit.suspendedLevelsIds}
+                              exerciseAccordion={exerciseAccordion}
+                              targetsList={targetsList}
+                              updateInfobarData={updateInfobarData}
+                              toggleAccordion={toggleAccordion}
+                            />
                           )}
                         </div>
                       )
                     )}
                 </div>
-              </div>
+              </AdminUnitHeader>
             </div>
           ))
         ) : (
