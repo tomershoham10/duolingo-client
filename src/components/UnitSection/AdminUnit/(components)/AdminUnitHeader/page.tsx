c906464301ae54eb1f68cf6435fd3fba5d62cae7
tomershoham10 <tomershoham10@gmail.com>
2024-09-30 14:44:57 +0300

    refactor: rearanged folders

--- src/components/UnitSection/AdminUnit/(components)/AdminUnitHeader/page.tsx
+++ src/components/UnitSection/AdminUnit/(components)/AdminUnitHeader/page.tsx
@@ -5,9 +5,11 @@ import { faBook } from '@fortawesome/free-solid-svg-icons';
 
 import { fieldToEditType } from '@/app/store/stores/useInfoBarStore';
 import Button, { ButtonColors } from '@/components/(buttons)/Button/page';
+import { ReactNode } from 'react';
 library.add(faBook);
 
 interface AdminUnitHeaderProps {
+  children: ReactNode;
   unit: UnitType;
   unitIndex: number;
   courseId: string;
@@ -23,43 +25,53 @@ interface AdminUnitHeaderProps {
 }
 
 const AdminUnitHeader: React.FC<AdminUnitHeaderProps> = (props) => {
-  const { unit, unitIndex, courseId, isSuspended, updateInfobarData } = props;
+  const {
+    children,
+    unit,
+    unitIndex,
+    courseId,
+    isSuspended,
+    updateInfobarData,
+  } = props;
   return (
-    <div className='grid-col-3 grid h-[6.5rem] max-h-[6.5rem] min-h-[6.5rem] w-full grid-flow-col grid-rows-2 items-center justify-between rounded-t-lg bg-duoGreen-default py-3 pl-4 text-white dark:bg-duoGrayDark-dark sm:h-fit'>
-      <button
-        className='col-span-1 flex-none cursor-pointer items-center justify-start text-xl font-extrabold'
-        onClick={() => {
-          updateInfobarData(
-            fieldToEditType.UNIT,
-            unit._id,
-            unitIndex,
-            unit.levelsIds,
-            courseId,
-            isSuspended
-          );
-        }}
-      >
-        <label className='cursor-pointer'>Unit {unitIndex + 1}</label>
-      </button>
-      <label className='col-span-2 w-[90%] items-center justify-center font-semibold'>
-        {unit.description}
-      </label>
-      <div className='row-span-2 mr-5 flex items-start justify-end'>
-        {unit.guidebookId ? (
-          <button className='flex w-40 cursor-pointer flex-row items-center justify-start rounded-2xl border-[2.5px] border-b-[4px] border-duoGreen-darker bg-duoGreen-button p-3 text-sm font-bold hover:border-duoGreen-dark hover:bg-duoGreen-default hover:text-duoGreen-textHover active:border-[2.5px]'>
-            <FontAwesomeIcon className='ml-2 mr-2 h-6 w-6' icon={faBook} />
-            <Link href={''}>
-              <label className='cursor-pointer items-center justify-center text-center'>
-                GUIDEBOOK
-              </label>
-            </Link>
-          </button>
-        ) : (
-          <div className=''>
-            <Button label={'CREATE GUIDEBOOK'} color={ButtonColors.WHITE} />
-          </div>
-        )}
+    <div className='flex-col'>
+      <div className='grid-col-3 grid h-[6.5rem] max-h-[6.5rem] min-h-[6.5rem] w-full grid-flow-col grid-rows-2 items-center justify-between rounded-t-lg bg-duoGreen-default py-3 pl-4 text-white dark:bg-duoGrayDark-dark sm:h-fit'>
+        <button
+          className='col-span-1 flex-none cursor-pointer items-center justify-start text-xl font-extrabold'
+          onClick={() => {
+            updateInfobarData(
+              fieldToEditType.UNIT,
+              unit._id,
+              unitIndex,
+              unit.levelsIds,
+              courseId,
+              isSuspended
+            );
+          }}
+        >
+          <label className='cursor-pointer'>Unit {unitIndex + 1}</label>
+        </button>
+        <label className='col-span-2 w-[90%] items-center justify-center font-semibold'>
+          {unit.description}
+        </label>
+        <div className='row-span-2 mr-5 flex items-start justify-end'>
+          {unit.guidebookId ? (
+            <button className='flex w-40 cursor-pointer flex-row items-center justify-start rounded-2xl border-[2.5px] border-b-[4px] border-duoGreen-darker bg-duoGreen-button p-3 text-sm font-bold hover:border-duoGreen-dark hover:bg-duoGreen-default hover:text-duoGreen-textHover active:border-[2.5px]'>
+              <FontAwesomeIcon className='ml-2 mr-2 h-6 w-6' icon={faBook} />
+              <Link href={''}>
+                <label className='cursor-pointer items-center justify-center text-center'>
+                  GUIDEBOOK
+                </label>
+              </Link>
+            </button>
+          ) : (
+            <div className=''>
+              <Button label={'CREATE GUIDEBOOK'} color={ButtonColors.WHITE} />
+            </div>
+          )}
+        </div>
       </div>
+      {children}
     </div>
   );
 };
