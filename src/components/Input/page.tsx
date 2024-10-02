54c94b57ec7b93875f59195a7f4b37b0de5bb59c
tomershoham10 <tomershoham10@gmail.com>
2024-10-02 08:31:09 +0300

    refactor: props. ... -> const {...} = props

--- src/components/Input/page.tsx
+++ src/components/Input/page.tsx
@@ -7,22 +7,21 @@ export enum InputTypes {
   PASSWORD = 'password',
 }
 
-const Input: React.FC<InputProps> = (props: InputProps) => {
+const Input: React.FC<InputProps> = (props) => {
+  const { type, name, placeholder, value, onChange, className, failed } = props;
   return (
-    <div className={`relative ${props.className} w-full`}>
+    <div className={`relative ${className} w-full`}>
       <input
-        type={props.type}
-        name={props.name}
+        type={type}
+        name={name}
         className={`text:sm h-10 w-full rounded-xl border-2 bg-duoGray-lighter px-1 py-2 font-bold text-duoGray-darkest focus:outline-none dark:border-duoGrayDark-light dark:bg-duoGrayDark-dark dark:text-duoGrayDark-lightest dark:focus:border-duoBlueDark-text lg:h-14 lg:p-3 lg:text-xl ${
-          props.failed ? 'border-duoRed-light' : 'border-duoGray-default'
+          failed ? 'border-duoRed-light' : 'border-duoGray-default'
         }`}
-        placeholder={props.placeholder}
-        value={props.value}
-        onChange={(e) =>
-          props.onChange ? props.onChange(e.target.value) : null
-        }
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange && onChange(e.target.value)}
       />
-      {props.failed && (
+      {failed && (
         <FontAwesomeIcon
           icon={faExclamation}
           className='absolute right-3 top-1/2 w-4 -translate-y-1/2 transform rounded-full border-2 border-duoRed-default p-[0.1rem] text-duoRed-default'
