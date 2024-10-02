450d17a60f568257209033f53d3ab2255bb47601
tomershoham10 <tomershoham10@gmail.com>
2024-10-02 08:29:34 +0300

    feat: TargetsDropdowns + getExercisesByModelId

--- src/app/(popups)/(syllabus)/addExercises/page.tsx
+++ src/app/(popups)/(syllabus)/addExercises/page.tsx
@@ -7,8 +7,14 @@ import PopupHeader, { PopupSizes } from '../../PopupHeader/page';
 import { PopupsTypes } from '@/app/store/stores/usePopupStore';
 import { useStore } from 'zustand';
 import { useInfoBarStore } from '@/app/store/stores/useInfoBarStore';
-import Pagination from '@/components/Navigation/Pagination/page';
 import TargetsDropdowns from '@/components/TargetsDropdowns';
+import { getExercisesByModelId } from '@/app/API/classes-service/exercises/functions';
+import { useCallback, useState } from 'react';
+import pRetry from 'p-retry';
+import Table from '@/components/Table/page';
+import Link from 'next/link';
+import RoundButton from '@/components/RoundButton';
+import { TiPlus } from 'react-icons/ti';
 
 const AddExercises: React.FC = () => {
   const lessonId = useStore(useInfoBarStore, (state) => state.syllabusFieldId);
@@ -17,6 +23,19 @@ const AddExercises: React.FC = () => {
     (state) => state.syllabusFieldIndex
   );
 
+  const [exercisesList, setExercisesList] = useState<ExerciseType[] | null>(
+    null
+  );
+
+  const fetchExercises = useCallback(async (model: TargetType | null) => {
+    if (model) {
+      const exercises = await pRetry(() => getExercisesByModelId(model._id), {
+        retries: 5,
+      });
+      setExercisesList(exercises);
+    }
+  }, []);
+
   return (
     <PopupHeader
       popupType={PopupsTypes.ADD_EXERCISES}
@@ -24,7 +43,39 @@ const AddExercises: React.FC = () => {
       header={`lesson no. ${lessonIndex + 1}`}
       onClose={() => {}}
     >
-      <TargetsDropdowns />
+      <TargetsDropdowns
+        excludeFileType={true}
+        onModelSelected={fetchExercises}
+      />
+      {exercisesList && (
+        <section className='mx-auto w-fit pt-6'>
+          <Table
+            headers={[
+              { key: 'type', label: 'exercise type' },
+              { key: 'adminComments', label: 'comments' },
+              { key: 'link', label: 'preview' },
+              { key: 'add', label: 'add' },
+            ]}
+            rows={exercisesList.map((exercise) => {
+              return {
+                type: exercise.type,
+                adminComments: exercise.adminComments,
+                link: (
+                  <Link
+                    className='w-fit cursor-pointer gap-2 text-center text-duoBlue-default hover:text-duoBlue-default dark:text-duoPurpleDark-default dark:hover:opacity-90'
+                    href={`${`/classroom/exercise-preview/${exercise._id}`}`}
+                    target='_blank'
+                  >
+                    preview
+                  </Link>
+                ),
+                add: <RoundButton Icon={TiPlus} onClick={() => {}} />,
+              };
+            })}
+            isLoading={false}
+          />
+        </section>
+      )}
     </PopupHeader>
   );
 };
